03c166370fe4e01e75c5939995f0542595e3a576
aaronpoweruser <atlrudeboi1@gmail.com>
2013-11-01 18:48:01 -0400

    4.4 Bring up
    * Add device configs
    * Add prebuilt products
    
    Change-Id: I371a89f4da7e8af6c7bf35a95080a41c78bba740

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2013 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (pa_deb,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_deb.mk
+endif
+ifeq (pa_flo,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_flo.mk
+endif
+ifeq (pa_grouper,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_grouper.mk
+endif
+ifeq (pa_hammerhead,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_hammerhead.mk
+endif
+ifeq (pa_maguro,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_maguro.mk
+endif
+ifeq (pa_mako,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_mako.mk
+endif
+ifeq (pa_manta,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_manta.mk
+endif
+ifeq (pa_tilapia,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_tilapia.mk
+endif
+
