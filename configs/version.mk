0b3c7fb7c9001b67f89d0d0e92e62b9a0827627a
Yamil Ghazi <yghazi@gmail.com>
2013-11-08 05:24:47 -0600

    Show date in ota name. Delete unused mk
    Change-Id: If08d330f6f14ca832f6ebfacfe00001a64319b1d

--- configs/version.mk
+++ configs/version.mk
@@ -1,8 +1,8 @@
 VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)$(ROM_VERSION_MAINTENANCE)
 ifeq ($ROM_VERSION_TAG,"")
-	export ROM_VERSION := $(VERSION)-($ROM_VERSION_TAG)
+	export ROM_VERSION := $(VERSION)-($ROM_VERSION_TAG)-$(shell date -u +%Y%m%d)
 else 
-	export ROM_VERSION := $(VERSION)
+	export ROM_VERSION := $(VERSION)-$(shell date -u +%Y%m%d)
 endif 
 
 PRODUCT_PROPERTY_OVERRIDES += \
