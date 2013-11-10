a7ac786fea33e45a2cee84f35cd2101bba763a40
BigBrother1984 <stevewatersy@gmail.com>
2013-11-10 04:02:51 +0100

    Not yet ready for this. Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- configs/pa_overrides.mk
+++ configs/pa_overrides.mk
@@ -16,14 +16,14 @@ PRODUCT_PACKAGES += \
         Busybox
 
 # Source or prebuilt PA-prefs logic
-ifneq ($(PREFS_FROM_SOURCE),true)
-    PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk
-else
-    # Build paprefs from sources
-    PRODUCT_PACKAGES += \
-        ParanoidPreferences
-endif
+#ifneq ($(PREFS_FROM_SOURCE),true)
+#    PRODUCT_COPY_FILES += \
+#        vendor/pa/prebuilt/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk
+#else
+#    # Build paprefs from sources
+#    PRODUCT_PACKAGES += \
+#        ParanoidPreferences
+#endif
 
 # Include ParanoidOTA
 ifneq ($(NO_OTA_BUILD),true)
