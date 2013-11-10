9f8d80da568419238bc7dd631560dff798ae97ce
Evan Anderson <evan1124@gmail.com>
2013-11-10 00:19:56 -0500

    Add su support
    Using chainfire's supersu
    
    Change-Id: Ia6294c121694380a432020b959c163af95653af9
    Signed-off-by: Evan Anderson <evan1124@gmail.com>

--- configs/system.mk
+++ configs/system.mk
@@ -34,5 +34,8 @@ PRODUCT_COPY_FILES += \
     vendor/$(VENDOR)/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/$(VENDOR)/prebuilt/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
 
-
-
+# SU Support
+PRODUCT_COPY_FILES += \
+    vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/su \
+    vendor/$(VENDOR)/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/$(VENDOR)/prebuilt/apk/Superuser.apk:system/app/Superuser.apk
