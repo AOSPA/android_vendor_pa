fd47b1f1661f43f0b5c996b82b745581c1c3bf20
beerbong <yghazi@gmail.com>
2013-11-11 13:46:57 +0100

    Get ROM_VERSION_TAG correctly
    Change-Id: I9239ca0db1e157cad666a426773ee3941a7232e4

--- configs/version.mk
+++ configs/version.mk
@@ -1,6 +1,6 @@
 VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)$(ROM_VERSION_MAINTENANCE)
-ifeq ($ROM_VERSION_TAG,"")
-	export ROM_VERSION := $(VERSION)-($ROM_VERSION_TAG)-$(shell date -u +%Y%m%d)
+ifneq ($ROM_VERSION_TAG,"")
+	export ROM_VERSION := $(VERSION)-$(ROM_VERSION_TAG)-$(shell date -u +%Y%m%d)
 else 
 	export ROM_VERSION := $(VERSION)-$(shell date -u +%Y%m%d)
 endif 
