7bb6870ddd9551ded4f4228a57ec2b72cb60d8d3
Sushanth Somayaji <rksomayaji@gmail.com>
2014-01-04 19:43:22 +0530

    Changed the options and output directory.
    Added a way to parse multiple options while building such as clean, threads and sync together. Output directory can be exported by declaring OUT_DIR_COMMON_BASE variable.
    
    Change-Id: Ib45c7e5a35eaa42e028fd41c39b1854d009467e4

--- build.sh
+++ build.sh
@@ -8,8 +8,17 @@ clear
 
 # Get current path
 DIR="$(cd `dirname $0`; pwd)"
-OUT="$(readlink $DIR/out)"
-[ -z "${OUT}" ] && OUT="${DIR}/out"
+
+# **Inspired from AOSP** Out directory can be exported by the variable exported by
+# the command "export OUT_DIR_COMMON_BASE=/path/to/out/directory" This will make a
+# folder in the specified output directory with the name of the source directory.
+if [ -z $OUT_DIR_COMMON_BASE];
+then
+	OUT="$(readlink $DIR/out)"
+	[ -z "${OUT}" ] && OUT="${DIR}/out"
+else
+	OUT=$OUT_DIR_COMMON_BASE
+fi
 
 # Prepare output customization commands
 red=$(tput setaf 1)             #  red
@@ -28,16 +37,22 @@ txtrst=$(tput sgr0)             # Reset
 : ${THREADS:="$(cat /proc/cpuinfo | grep "^processor" | wc -l)"}
 
 # If there is more than one jdk installed, use latest 6.x
-if [ "`update-alternatives --list javac | wc -l`" -gt 1 ]; then
-        JDK6=$(dirname `update-alternatives --list javac | grep "\-6\-"` | tail -n1)
-        JRE6=$(dirname ${JDK6}/../jre/bin/java)
-        export PATH=${JDK6}:${JRE6}:$PATH
-fi
+function update-alt {
+	if [ "`update-alternatives --list javac | wc -l`" -gt 1 ]; then
+	        JDK6=$(dirname `update-alternatives --list javac | grep "\-6\-"` | tail -n1)
+	        JRE6=$(dirname ${JDK6}/../jre/bin/java)
+	        export PATH=${JDK6}:${JRE6}:$PATH
+	fi
+}
+
+type -P update-alternatives &>/dev/null && update-alt
+
 JVER=$(javac -version  2>&1 | head -n1 | cut -f2 -d' ')
 
 # Import command line parameters
 DEVICE="$1"
-EXTRAS="$2"
+EXTRAS=("$@")
+EXTRAS=(${EXTRAS[@]:1})
 
 # Get build version
 MAJOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR := *' | sed  's/ROM_VERSION_MAJOR := //g')
@@ -46,44 +61,52 @@ MAINTENANCE=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE := *'
 
 VERSION=$MAJOR.$MINOR$MAINTENANCE
 
-# If there is no extra parameter, reduce parameters index by 1
-if [ "$EXTRAS" == "true" ] || [ "$EXTRAS" == "false" ]; then
-        SYNC="$2"
-        UPLOAD="$3"
-else
-        SYNC="$3"
-        UPLOAD="$4"
+# Usage details for the command by sending help in the place of device name
+if [ $DEVICE == "help" ]; then
+	echo "${bldcya}AOSPA Script for building PA ROM.${txtrst}"
+	echo ""
+	echo "Usage: ./rom-build.sh [device name] (true/false) (threads N) (clean/cclean)"
+	echo ""
+	echo ""
+	echo "   device name	:   The name of the device. Only Nexus device and OPPO devices are officially supported by PA Team. For other devices try AOSPA-legacy"
+	echo "   help		:   Displays this message and exits."
+	echo "   true/false	:   Optional. Whether to sync prior to build."
+	echo "   threads N	:   Optional. Sync with N threads."
+	echo "   clean/cclean	:   Optional. Clean previously formed intermediates and output files."
+	echo ""
+	echo ""
+	echo "Out directory can be exported by the variable exported by the command "export OUT_DIR_COMMON_BASE=/path/to/out/directory" This will make a folder in the specified output directory with the name of the source directory."
+	echo "For further details Paranoid Android Google Plus Community: https://plus.google.com/u/0/communities/112514149478109338346, XDA Community: http://forum.xda-developers.com/paranoid-android, github repo: https://github.com/AOSPA. Tutorial to build AOSPA http://forum.xda-developers.com/showthread.php?t=1863547."
+	exit 0
 fi
 
-# Get start time
-res1=$(date +%s.%N)
-
-echo -e "${cya}Building ${bldcya}AOSPA $VERSION for $DEVICE ${txtrst}";
-echo -e "${bldgrn}Start time: $(date) ${txtrst}"
-
-# Decide what command to execute
-case "$EXTRAS" in
-        threads)
-                echo -e "${bldblu}Please enter desired building/syncing threads number followed by [ENTER]${txtrst}"
-                read threads
-                THREADS=$threads
-        ;;
-        clean|cclean)
-                echo -e "${bldblu}Cleaning intermediates and output files${txtrst}"
-                export CLEAN_BUILD="true"
-                [ -d "${DIR}/out" ] && rm -Rf ${DIR}/out/*
-        ;;
-esac
+# If extra commands are given decide what command to execute prior to build
+if [ ! -z $EXTRAS ]; then
+	for opt in "${EXTRAS[@]}"
+	do
+	count=$((count+1))
+	case $opt in
+		clean|cclean)
+			echo -e "${bldblu}Cleaning intermediates and output files${txtrst}"
+			export CLEAN_BUILD="true"
+	                [ -d $OUT ] & [ "$(ls -A $OUT)" ] && rm -Rf $OUT/*
+	        ;;
+		threads)
+	                THREADS=${myArray[count]}
+			echo $THREADS
+		;;
+# Fetch latest sources
+		true)
+			echo -e ""
+		        echo -e "${bldblu}Fetching latest sources${txtrst}"
+		        repo sync -j"$THREADS"
+		        echo -e ""	
+		;;
+	esac
+	done
+fi
 
 echo -e ""
-
-# Fetch latest sources
-if [ "$SYNC" == "true" ]; then
-        echo -e ""
-        echo -e "${bldblu}Fetching latest sources${txtrst}"
-        repo sync -j"$THREADS"
-        echo -e ""
-fi
 
 if [ ! -r "${DIR}/out/versions_checked.mk" ] && [ -n "$(java -version 2>&1 | grep -i openjdk)" ]; then
         echo -e "${bldcya}Your java version still not checked and is candidate to fail, masquerading.${txtrst}"
