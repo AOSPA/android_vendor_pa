dfe71948032364bbf644f51a21ce14b9f77774a7
BigBrother1984 <stevewatersy@gmail.com>
2014-01-19 02:39:40 +0100

    Move bootanimation selection to its own makefile..
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- configs/pa_overrides.mk
+++ configs/pa_overrides.mk
@@ -31,12 +31,3 @@ ifneq ($(NO_OTA_BUILD),true)
     PRODUCT_PACKAGES += \
         ParanoidOTA
 endif
-
-# Include bootanimation
-ifneq ($(PARANOID_BOOTANIMATION_NAME),)
-    PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/bootanimation/$(PARANOID_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
-else
-    PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/bootanimation/XHDPI.zip:system/media/bootanimation.zip
-endif
