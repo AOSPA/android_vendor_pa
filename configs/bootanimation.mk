189eb504c70e0d7de0973cbabe87c6b834e94a96
D4rKn3sSyS <jesdga95@gmail.com>
2014-01-19 18:48:57 -0500

    Fix vendor and rename makefile
    Change-Id: Icfdc94c7465f380959b13e57da48566ccdd16e2c

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Add PA bootanimation based on device resolution
-ifeq ((pa_maguro,$(TARGET_PRODUCT)))
+ifeq (pa_maguro,$(TARGET_PRODUCT))
     PRODUCT_COPY_FILES += \
         vendor/pa/prebuilt/bootanimation/1280x720.zip:system/media/bootanimation.zip
 endif
