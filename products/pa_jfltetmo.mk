44664420d715e2fc22ce9a283323ae3bb0cd2cc7
Andre Saddler <dresadd09691@gmail.com>
2014-01-22 13:46:42 -0500

    products: jfltetmo: update build fingerprint
    Signed-off-by: Andre Saddler <dresadd09691@gmail.com>

--- products/pa_jfltetmo.mk
+++ products/pa_jfltetmo.mk
@@ -37,6 +37,6 @@ PRODUCT_DEVICE := jfltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SGH-M919
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.3/JSS15J/M919UVUEMK2:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.3 JSS15J M919UVUEMK2 release-keys"
 
 endif
