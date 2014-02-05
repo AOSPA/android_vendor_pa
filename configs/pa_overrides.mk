c827e49280584c6553c7c1ec2a732f5146617c55
houst0nn <houstonn@gmail.com>
2014-02-05 02:44:36 -0500

    Allow override of DUN settings
    Allow override of system DUN settings by setting
    persist.sys.dun.override
    to one of the following values:
    2 = not set, 0 = DUN not required, 1 = DUN required
    If the prop is not set or is set to an invalid value the system setting
    will be used.
    
    Change-Id: I55066334d66ce24a485fbbb2cc1e6450c1025507

--- configs/pa_overrides.mk
+++ configs/pa_overrides.mk
@@ -40,3 +40,8 @@ endif
 # Extra properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pa.device=$(DEVICE)
+
+# Allow override of system DUN
+# 2 = not set, 0 = DUN not required, 1 = DUN required
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dun.override=0
