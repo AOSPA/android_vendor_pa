43a2bd2b7953ee80aa17b3c86e38d41c2f80c991
Parthipan Ramesh <parthipan.ramesh@gmail.com>
2014-03-28 15:43:44 +0100

    PA-Vendor: Built-in superuser
    Change-Id: I9f21ed8bcc8db6fee25b7a6ab8f86ae342242a85
    Signed-off-by: Parthipan Ramesh <parthipan.ramesh@gmail.com>
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- configs/system.mk
+++ configs/system.mk
@@ -35,14 +35,15 @@ PRODUCT_COPY_FILES += \
     vendor/$(VENDOR)/prebuilt/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
 
 # SU Support
-PRODUCT_COPY_FILES += \
-    vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/daemonsu \
-    vendor/$(VENDOR)/prebuilt/bin/su:system/xbin/su \
-    vendor/$(VENDOR)/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
-    vendor/$(VENDOR)/prebuilt/apk/Superuser.apk:system/app/Superuser.apk
+SUPERUSER_EMBEDDED := true
+
+PRODUCT_PACKAGES := \
+    Superuser \
+    su
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+    ro.build.selinux=1 \
+    persist.sys.root_access=3
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
