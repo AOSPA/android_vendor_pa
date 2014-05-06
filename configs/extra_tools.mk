f809b771be177b1a3f78290e5e59425263a6e51f
houst0nn <houstonn@gmail.com>
2014-05-06 04:25:15 -0400

    Add exFat support
    Change-Id: Ia36bba67539391fc9cb39ac860b0d2683328a20b
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- configs/extra_tools.mk
+++ configs/extra_tools.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 ParanoidAndroid Project
+# Copyright (C) 2014 ParanoidAndroid Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,13 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-include vendor/pa/vendor.mk
-include vendor/pa/configs/aosp_fixes.mk
-include vendor/pa/configs/system.mk
-include vendor/pa/configs/pa_overrides.mk
-include vendor/pa/configs/bootanimation.mk
-include vendor/pa/configs/version.mk
-include vendor/pa/configs/telephony.mk
-include vendor/pa/configs/extra_tools.mk
+#Extra tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat
