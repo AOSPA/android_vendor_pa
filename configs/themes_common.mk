9c7688715365cb637e8c0f2df6dce7cb494acd20
Yamil Ghazi <yghazi@gmail.com>
2014-05-11 20:24:13 +0200

    CM Theme Engine [4/8]
    Build Theme Engine
    
    PS2: build ThemeManager
    
    Change-Id: I70f84075ee7ce5bbe7abbd9033669a5a110a61d3

---
+++ configs/themes_common.mk
@@ -0,0 +1,9 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider \
+    ThemeManager
+
+PRODUCT_COPY_FILES += \
+    vendor/pa/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/pa/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
