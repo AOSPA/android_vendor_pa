6b877e0d716b1a8c22ba365ef1224d6703d3a8b2
Evan Anderson <evananderson@aospa.co>
2014-11-14 20:59:01 -0500

    Remove traces of gnex
    Change-Id: I8490a8d3e6d1a8090463c3492daa7321952e7677
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,3 @@ add_lunch_combo pa_hammerhead-userdebug
 add_lunch_combo pa_tilapia-userdebug
 add_lunch_combo pa_mako-userdebug
 add_lunch_combo pa_manta-userdebug
-add_lunch_combo pa_maguro-userdebug
-add_lunch_combo pa_toro-userdebug
-add_lunch_combo pa_toroplus-userdebug
