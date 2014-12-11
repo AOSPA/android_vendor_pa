7feb4ffa6771b10b1d55ab496667ef9f61a4f765
ayysir <ayysir@aospal.com>
2014-12-10 19:35:57 -0500

    vendor: Add Shamu "Nexus 6"
    Change-Id: I8dd2ab8553ab39e9ce64a6c51621b5466144ef74

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -17,7 +17,7 @@ ifneq ($(filter pa_mako pa_grouper pa_tilapia,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/pa/prebuilt/bootanimation/1280x720.zip:system/media/bootanimation.zip
 endif
-ifeq (pa_hammerhead,$(TARGET_PRODUCT))
+ifeq ($(filter pa_hammerhead pa_shamu,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/pa/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
 endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo pa_deb-userdebug
 add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_flo-userdebug
 add_lunch_combo pa_hammerhead-userdebug
+add_lunch_combo pa_shamu-userdebug
 add_lunch_combo pa_tilapia-userdebug
 add_lunch_combo pa_mako-userdebug
 add_lunch_combo pa_manta-userdebug
