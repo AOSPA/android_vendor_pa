a84222706ee69ed57deca2e95c2e30f542b9c600
BigBrother1984 <carlosavignano@aospa.co>
2014-12-22 00:34:06 +0100

    vendor: Use default lollipop bootanimation
    * Till we make our (new?) one.
    * Thanks to guerreromanuel at XDA for 60fps files:
      http://forum.xda-developers.com/android/themes/bootanimation-android-l-60fps-v3-t2918168
    
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -21,11 +21,12 @@ ifeq ($(filter pa_hammerhead pa_shamu,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/pa/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
 endif
+# Hack: Use 1920x1080 of stock one till we get size for all of them.
 ifneq ($(filter pa_deb pa_flo,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/bootanimation/1920x1200.zip:system/media/bootanimation.zip
+        vendor/pa/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
 endif
 ifeq (pa_manta,$(TARGET_PRODUCT))
     PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/bootanimation/2560x1600.zip:system/media/bootanimation.zip
+        vendor/pa/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
 endif
