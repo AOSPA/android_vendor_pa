7658d96e658b9eb54945b3d22af1e63ef5639e97
ayysir <ayysir@aospal.com>
2014-12-24 14:30:01 -0500

    Shamu: fix inheritence
    Change-Id: Ifc2ff38e0f0ee601439a54dcb1fa8f2f609ad663

--- products/pa_shamu.mk
+++ products/pa_shamu.mk
@@ -23,7 +23,7 @@ OVERLAY_TARGET := pa_xxhdpi
 include vendor/pa/main.mk
 
 # Inherit AOSP device configuration
-$(call inherit-product, device/moto/shamu/full_shamu.mk)
+$(call inherit-product, device/motorola/shamu/full_shamu.mk)
 
 # Override AOSP build properties
 PRODUCT_NAME := pa_shamu
