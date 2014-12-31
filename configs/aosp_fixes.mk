6c355f2822d159697cc92e911b520554e0ca564a
ayysir <ayysir@aospal.com>
2014-12-31 12:21:38 -0500

    Revert swype required lib to old naming
    Change-Id: Icdda56d39f144d1fd588d6e49881c54576aaa947

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -34,4 +34,4 @@ PRODUCT_COPY_FILES += \
 
 # Proprietary latinime lib needed for Keyboard swyping
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+    vendor/pa/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
