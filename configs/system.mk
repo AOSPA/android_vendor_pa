1992bbd842306deafff09926b6d61e300bed84f1
Evan Anderson <evananderson@aospa.co>
2015-01-10 01:33:30 -0500

    configs: Strip out su related lines
    We will not be shipping with su anymore
    
    Change-Id: If5780f2cdccbc64c142f80f80c55c4da84b57625
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- configs/system.mk
+++ configs/system.mk
@@ -16,10 +16,6 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/$(VENDOR)/overlay/common
 PRODUCT_PACKAGE_OVERLAYS += vendor/$(VENDOR)/overlay/$(TARGET_PRODUCT)
 
-# Copy custom ramdisk
-#PRODUCT_COPY_FILES += \
-#    vendor/$(VENDOR)/prebuilt/etc/init.$(VENDOR).rc:root/init.$(VENDOR).rc
-
 # init.d script support
 PRODUCT_COPY_FILES += \
     vendor/$(VENDOR)/prebuilt/bin/sysinit:system/bin/sysinit
@@ -37,13 +33,3 @@ PRODUCT_COPY_FILES += \
 #    vendor/$(VENDOR)/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
 #    vendor/$(VENDOR)/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
 #    vendor/$(VENDOR)/prebuilt/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
-
-# SU Support
-#SUPERUSER_EMBEDDED := true
-
-#PRODUCT_PACKAGES := \
-#    Superuser \
-#    su
-
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    persist.sys.root_access=3
