f8adc2bf773d6413b674e37110dd6bdd86b2aa47
ayysir <ayysir@aospal.com>
2015-01-20 00:39:03 -0500

    vendor: global sepolicy inclusion (2/2)
    Signed-off-by: ayysir <ayysir@aospal.com>
    Change-Id: I57af7bbc387bbcfdd07c7e6d861af49118784e5b

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,13 @@
+#
+# This policy configuration will be used by all products that
+# inherit from vendor
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/pa/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    service_contexts \
+    system_server.te
