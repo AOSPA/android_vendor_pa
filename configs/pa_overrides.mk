b41dd4ec9e4e583549914db8161a665c275d15be
Valter Strods <valterstrods@aospa.co>
2015-01-23 02:17:45 +0200

    PA: Clean up makefile comments
    The PA overrides makefile got a facelift with a copyright year
    bump and it dropped a bunch of commented out code as we got version
    control with history and cool tools to get it back if we have to.
    
    Change-Id: Ic87d39f07eb8743ce9a8a00fcf16fe5aa92c7cf5

--- configs/pa_overrides.mk
+++ configs/pa_overrides.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 ParanoidAndroid Project
+# Copyright (C) 2015 ParanoidAndroid Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#PRODUCT_PACKAGES += \
-#    Busybox
-
 # Include ParanoidOTA
 ifneq ($(NO_OTA_BUILD),true)
     PRODUCT_PACKAGES += \
         ParanoidOTA
 endif
-
-# Extra properties
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.pa.device=$(DEVICE)
