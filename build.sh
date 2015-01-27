da59157305605e2349a25163093407e88b3e70e5
Valter Strods <valterstrods@aospa.co>
2015-01-27 21:53:08 +0200

    PA: Rewrite the build script
    usage: ./rom-build.sh <device> [sync] [user] [clean]
    
    The parameters are now accepted in any order so you do not need to
    worry about crazyness like that. The interactive mode was dropped
    as it was not referenced from anywhere, along with UPLOAD and
    CLEAN_BUILD variables being set. Various other changes have taken
    place all around, generally aiming for improved stability. As an
    extra on top, we have the username set correctly once again with
    this change applied.
    
    Change-Id: I52db5ee034b90f99e0f0501583073b2bca0fc0e1

--- build.sh
+++ build.sh
@@ -1,143 +1,154 @@
 #!/bin/bash
+# PA5 build helper script
 
-# Version 2.0.4, Adapted for AOSPA.
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
 
-# We don't allow scrollback buffer
+# Nuke scrollback
 echo -e '\0033\0143'
 clear
 
-# Get current path
-DIR="$(cd `dirname $0`; pwd)"
-OUT="$(readlink $DIR/out)"
-[ -z "${OUT}" ] && OUT="${DIR}/out"
+# Set up paths
+cd $(dirname $0)
+DIR_ROOT=$(pwd)
+DIR_OUT=$(readlink $DIR_ROOT/out)
+[ -z "$DIR_OUT" ] && DIR_OUT="$DIR_ROOT/out"
 
-# Prepare output customization commands
-red=$(tput setaf 1)             #  red
-grn=$(tput setaf 2)             #  green
-blu=$(tput setaf 4)             #  blue
-cya=$(tput setaf 6)             #  cyan
-txtbld=$(tput bold)             # Bold
-bldred=${txtbld}$(tput setaf 1) #  red
-bldgrn=${txtbld}$(tput setaf 2) #  green
-bldblu=${txtbld}$(tput setaf 4) #  blue
-bldcya=${txtbld}$(tput setaf 6) #  cyan
-txtrst=$(tput sgr0)             # Reset
-
-# Local defaults, can be overriden by environment
-if [ `uname -s` == "Darwin" ]; then
-: ${THREADS:="$(sysctl -n machdep.cpu.core_count)"}
-else
-: ${THREADS:="$(cat /proc/cpuinfo | grep "^processor" | wc -l)"}
+# Make sure everything looks sane so far
+if [ ! -d "$DIR_ROOT/vendor/pa" ]; then
+        echo -e "${CLR_BLD_RED}error: insane root directory ($DIR_ROOT)${CLR_RST}"
+        exit 1
 fi
 
-# If there is more than one jdk installed, use latest 7.x
-if [ "`update-alternatives --list javac | wc -l`" -gt 1 ]; then
-        JDK7=$(dirname `update-alternatives --list javac | grep "\-7\-"` | tail -n1)
-        JRE7=$(dirname ${JDK7}/../jre/bin/java)
-        export PATH=${JDK7}:${JRE7}:$PATH
+# Pick the default thread count (allow overrides from the environment)
+if [ -z "$THREADS" ]; then
+        if [ "$(uname -s)" = 'Darwin' ]; then
+                export THREADS=$(sysctl -n machdep.cpu.core_count)
+        else
+                export THREADS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
+        fi
 fi
-JVER=$(javac -version  2>&1 | head -n1 | cut -f2 -d' ')
 
-# Import command line parameters
-DEVICE="$1"
-EXTRAS="$2"
+# Pick the best JDK 7, if more than one is installed
+if [ "$(update-alternatives --list javac | wc -l)" -gt 1 ]; then
+        JDK7_PATH=$(dirname $(update-alternatives --list javac | grep '\-7\-') | tail -n1)
+        JRE7_PATH=$(dirname $JDK7_PATH/../jre/bin/java)
+        export PATH=$JDK7_PATH:$JRE7_PATH:$PATH
+fi
 
-# Get build version
-MAJOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR := *' | sed  's/ROM_VERSION_MAJOR := //g')
-MINOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MINOR := *' | sed  's/ROM_VERSION_MINOR := //g')
-MAINTENANCE=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE := *' | sed  's/ROM_VERSION_MAINTENANCE := //g')
-TAG=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_TAG := *' | sed  's/ROM_VERSION_TAG := //g')
+# Grab the build version
+VERSION_MAJOR=$(cat $DIR_ROOT/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR := *' | sed 's/ROM_VERSION_MAJOR := //g')
+VERSION_MINOR=$(cat $DIR_ROOT/vendor/pa/vendor.mk | grep 'ROM_VERSION_MINOR := *' | sed 's/ROM_VERSION_MINOR := //g')
+VERSION_MAINTENANCE=$(cat $DIR_ROOT/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE := *' | sed 's/ROM_VERSION_MAINTENANCE := //g')
+VERSION_TAG=$(cat $DIR_ROOT/vendor/pa/vendor.mk | grep 'ROM_VERSION_TAG := *' | sed 's/ROM_VERSION_TAG := //g')
 
 if [ -n "$TAG" ]; then
-        VERSION=$MAJOR.$MINOR$MAINTENANCE-$TAG
+        VERSION=$VERSION_MAJOR.$VERSION_MINOR$VERSION_MAINTENANCE-$VERSION_TAG
 else
-        VERSION=$MAJOR.$MINOR$MAINTENANCE
+        VERSION=$VERSION_MAJOR.$VERSION_MINOR$VERSION_MAINTENANCE
 fi
 
-# If there is no extra parameter, reduce parameters index by 1
-if [ "$EXTRAS" == "true" ] || [ "$EXTRAS" == "false" ]; then
-        USER="$2"
-        SYNC="$3"
-        UPLOAD="$4"
-else
-        USER="$3"
-        SYNC="$4"
-        UPLOAD="$5"
+# Grab all the command-line parameters
+export DEVICE=$1
+shift
+
+if [ -z "$DEVICE" ]; then
+        echo -e "${CLR_BLD_RED}error: no device specified${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}usage: $0 <device>${CLR_RST}"
+        exit 1
 fi
 
-# Get start time
-res1=$(date +%s.%N)
+while [[ "$#" > 0 ]]; do
+        PARAM=$(echo ${1,,})
+        case $PARAM in
+        -c|--clean|c|clean)
+            FLAG_CLEAN_BUILD=y
+            ;;
+        -u|--user|u|user)
+            FLAG_USER_BUILD=y
+            ;;
+        -s|--sync|s|sync)
+            FLAG_SYNC=y
+            ;;
+        *)
+            echo -e "${CLR_CYA}warning: skipping unknown parameter: $1${CLR_RST}"
+            ;;
+        esac
+        shift
+done
 
-echo -e "${cya}Building ${bldcya}AOSPA $VERSION for $DEVICE ${txtrst}";
-echo -e "${bldgrn}Start time: $(date) ${txtrst}"
+# Prep for a clean build, if requested so
+if [ "$FLAG_CLEAN_BUILD" = 'y' ]; then
+        echo -e "${CLR_BLD_BLU}Cleaning output files left from old builds${CLR_RST}"
+        echo -e ""
+        rm -rf ${DIR_ROOT}/out/*
+fi
 
-# Decide what command to execute
-case "$EXTRAS" in
-        threads)
-                echo -e "${bldblu}Please enter desired building/syncing threads number followed by [ENTER]${txtrst}"
-                read threads
-                THREADS=$threads
-        ;;
-        clean|cclean)
-                echo -e "${bldblu}Cleaning intermediates and output files${txtrst}"
-                export CLEAN_BUILD="true"
-                [ -d "${DIR}/out" ] && rm -Rf ${DIR}/out/*
-        ;;
-esac
+# And sync up, if asked to
+if [ "$FLAG_SYNC" = 'y' ]; then
+        echo -e "${CLR_BLD_BLU}Downloading the latest source files${CLR_RST}"
+        echo -e ""
+        repo sync -j"$THREADS"
+fi
 
-echo -e ""
-
-export DEVICE=$DEVICE
-
-#Use Prebuilt Chromium
+# Enable pre-built Chromium
 export USE_PREBUILT_CHROMIUM=1
 
-# Fetch latest sources
-if [ "$SYNC" == "true" ]; then
+# Mask Java, if it seems to be faulty
+if [ ! -r "$DIR_ROOT/out/versions_checked.mk" ] && [ -n "$(java -version 2>&1 | grep -i openjdk)" ]; then
+        echo -e "${CLR_BLD_CYA}Your Java version has not been checked and is a candidate for failure. Masquerading.${CLR_RST}"
         echo -e ""
-        echo -e "${bldblu}Fetching latest sources${txtrst}"
-        repo sync -j"$THREADS"
-        echo -e ""
+        JAVA_VERSION="java_version=$(javac -version 2>&1 | head -n1 | cut -f2 -d' ')"
 fi
 
-if [ ! -r "${DIR}/out/versions_checked.mk" ] && [ -n "$(java -version 2>&1 | grep -i openjdk)" ]; then
-        echo -e "${bldcya}Your java version still not checked and is candidate to fail, masquerading.${txtrst}"
-        JAVA_VERSION="java_version=${JVER}"
-fi
+# Check the starting time (of the real build process)
+TIME_START=$(date +%s.%N)
 
-if [ -n "${INTERACTIVE}" ]; then
-        echo -e "${bldblu}Dropping to interactive shell${txtrst}"
-        echo -en "${bldblu}Remeber to lunch you device:"
-        if [ "${VENDOR}" == "pa" ]; then
-                echo -e "[${bldgrn}lunch pa_$DEVICE-userdebug${bldblu}]${txtrst}"
-        else
-                echo -e "[${bldgrn}lunch full_$DEVICE-userdebug${bldblu}]${txtrst}"
-        fi
-        bash --init-file build/envsetup.sh -i
+# Friendly logging to tell the user everything is working fine is always nice
+echo -e "${CLR_BLD_GRN}Building AOSPA $VERSION for $DEVICE${CLR_RST}"
+echo -e "${CLR_GRN}Start time: $(date)${CLR_RST}"
+echo -e ""
+
+# Initializationizing!
+echo -e "${CLR_BLD_BLU}Setting up the environment${CLR_RST}"
+echo -e ""
+. build/envsetup.sh
+echo -e ""
+
+# Lunch-time!
+echo -e "${CLR_BLD_BLU}Lunching $DEVICE${CLR_RST} ${CLR_CYA}(Including dependencies sync)${CLR_RST}"
+echo -e ""
+if [ "$FLAG_USER_BUILD" = 'y' ]; then
+        lunch "pa_$DEVICE-user"
 else
-        # Setup environment
-        echo -e ""
-        echo -e "${bldblu}Setting up environment${txtrst}"
-        . build/envsetup.sh
-        echo -e ""
-
-        # lunch/brunch device
-        echo -e "${bldblu}Lunching device [$DEVICE] ${cya}(Includes dependencies sync)${txtrst}"
-        if [ "$USER" == "user" ]; then
-                lunch "pa_$DEVICE-user";
-        else
-                lunch "pa_$DEVICE-userdebug";
-        fi
-
-        echo -e "${bldblu}Starting compilation${txtrst}"
-        if [ "$USER" == "user" ]; then
-                mka dist
-        else
-                mka bacon
-        fi
+        lunch "pa_$DEVICE-userdebug"
 fi
 echo -e ""
 
-# Get elapsed time
-res2=$(date +%s.%N)
-echo -e "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds)${txtrst}"
+# Build away!
+echo -e "${CLR_BLD_BLU}Starting compilation${CLR_RST}"
+echo -e ""
+if [ "$FLAG_USER_BUILD" = 'y' ]; then
+        mka dist
+else
+        mka bacon
+fi
+echo -e ""
+
+# Check the finishing time
+TIME_END=$(date +%s.%N)
+
+# Log those times at the end as a fun fact of the day
+echo -e "${CLR_BLD_GRN}Total time elapsed:${CLR_RST} ${CLR_GRN}$(echo "($TIME_END - $TIME_START) / 60" | bc) minutes ($(echo "$TIME_END - $TIME_START" | bc) seconds)${CLR_RST}"
+echo -e ""
