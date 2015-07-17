81fe83bc43709324e37b38f138d245ed7875134b
mattflaming <mattflaming@aospa.co>
2015-07-17 00:24:09 -0400

    Revert "Revert "main.mk: Whitspace removal""
    This reverts commit e8fef99b13814c5781c6c54961fdd0d3014a9174.
    
    Change-Id: Ief3626881ff743914aa087027fe307f67688f951

--- main.mk
+++ main.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
 include vendor/pa/vendor.mk
 include vendor/pa/configs/aosp_fixes.mk
 include vendor/pa/configs/system.mk
