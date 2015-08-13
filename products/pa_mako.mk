3e2a1770050b1e0b43f0e9c9d2c6601e924a2516
Valter Strods <valterstrods@aospa.co>
2015-08-13 14:28:20 +0300

    mako: Update the fingerprint to LMY48I
    Nothing too fancy going on here, just a boring minor maintenance
    commit bumping the build fingerprint from one 5.1.1 build to another
    passing through.
    
    Change-Id: I212c0428527d3e6e22dd61bb09eb0d866ff2d914

--- products/pa_mako.mk
+++ products/pa_mako.mk
@@ -28,7 +28,7 @@ PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=occam \
-	BUILD_FINGERPRINT="google/occam/mako:5.1.1/LMY47V/1836172:user/release-keys" \
-	PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY47V 1836172 release-keys"
+	BUILD_FINGERPRINT="google/occam/mako:5.1.1/LMY48I/2074855:user/release-keys" \
+	PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48I 2074855 release-keys"
 
 endif
