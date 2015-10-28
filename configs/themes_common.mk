76f54a55286691754a8f35d0ad28f09c6a36ddf3
d34d <clark@cyngn.com>
2015-10-27 17:39:05 -0700

    Themes: Include aapt on device
    Change-Id: I123ba9519dd8c3f31582f0847de31939bdf6ac04

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,7 +1,8 @@
 # Theme engine
 PRODUCT_PACKAGES += \
-   ThemeChooser \
-   ThemesProvider
+    aapt \
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
    vendor/pa/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
