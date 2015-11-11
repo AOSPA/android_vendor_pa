aa7182a0acef0bd16624ea08a0948858d2fae463
Alexis Rico <me@sferadev.com>
2015-11-11 17:24:22 +0000

    pa: Roomservice for Paranoid Android
    - Dependencies are now located on vendor/pa/dependencies
    - Also add Sony devices
    
    Change-Id: Ifda16858581f0df1d62ba8e070909eb89060cc5c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+# Nexus
 add_lunch_combo pa_deb-userdebug
 add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_flo-userdebug
@@ -7,3 +8,33 @@ add_lunch_combo pa_shamu-userdebug
 add_lunch_combo pa_tilapia-userdebug
 add_lunch_combo pa_mako-userdebug
 add_lunch_combo pa_manta-userdebug
+
+# Sony Yukon Platform
+add_lunch_combo pa_eagle-userdebug
+add_lunch_combo pa_flamingo-userdebug
+add_lunch_combo pa_seagull-userdebug
+add_lunch_combo pa_tianchi-userdebug
+
+# Sony Rhine Platform
+add_lunch_combo pa_amami-userdebug
+add_lunch_combo pa_honami-userdebug
+add_lunch_combo pa_togari-userdebug
+
+# Sony Shinano Platform
+add_lunch_combo pa_aries-userdebug
+add_lunch_combo pa_castor-userdebug
+add_lunch_combo pa_castor_windy-userdebug
+add_lunch_combo pa_leo-userdebug
+add_lunch_combo pa_scorpion-userdebug
+add_lunch_combo pa_scorpion_windy-userdebug
+add_lunch_combo pa_sirius-userdebug
+
+# Sony Kitakami Platform
+add_lunch_combo pa_ivy-userdebug
+add_lunch_combo pa_karin-userdebug
+add_lunch_combo pa_karin_windy-userdebug
+add_lunch_combo pa_sumire-userdebug
+add_lunch_combo pa_suzuran-userdebug
+
+# CAF
+add_lunch_combo pa_bacon-userdebug
