854e84a551e8ddc17318b7387e3544f81130039d
Evan Anderson <evananderson@aospa.co>
2015-11-12 20:18:47 -0500

    configs: Remove swype lib for now
    Needs updating and modified handling for 64bit devices.
    
    Change-Id: I74bd84c0ffbeb72c70921fb4cd7c1ec952822e0c
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -33,5 +33,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # Proprietary latinime lib needed for Keyboard swyping
-PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+#PRODUCT_COPY_FILES += \
+#    vendor/pa/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
