2b169b7ca187afcdeb27b59bbd5db24fd7ac5ba6
Alexis Rico <me@sferadev.com>
2015-11-27 16:47:13 +0000

    pa: Add ivy (Sony Z3+)
    Change-Id: I7e1991161045300da7280c39503467f6c27c1009

---
+++ products/ivy/pa_ivy.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2015 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_ivy,$(TARGET_PRODUCT))
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/main.mk
+
+include vendor/twrp-sony/products/kitakami/ivy/ivy.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/sony/ivy/aosp_e6553.mk)
+
+# Sony uses mkqcdtbootimg
+BOARD_CUSTOM_BOOTIMG := true
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_ivy
+PRODUCT_MODEL := Xperia Z3+ (AOSPA)
+
+# Keep these values synchronised with the firmware versions used by
+# AOSP blobs. Do NOT update blobs (and therefore these values) from
+# a new factory image. Sony has provided AOSP specific blobs to use
+# with AOSP; blobs from a factory image will NOT work.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E6553 \
+                                BUILD_FINGERPRINT="Sony/E6553/E6553:5.0.2/28.0.A.8.251/1150652000:user/release-keys" \
+                                PRIVATE_BUILD_DESC="E6553-user 5.0.2 28.0.A.8.251 1150652000 release-keys"
+
+endif
