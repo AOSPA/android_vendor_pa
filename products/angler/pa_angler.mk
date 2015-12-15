fec50e86991a44f18604fdfe513e3e1024260b0c
Evan Anderson <evananderson@aospa.co>
2015-12-14 19:18:50 -0600

    angler: Update build fingerprint
    Change-Id: I94d0c9c241cc0f419ee6bb9856d25dd929c8495f
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- products/angler/pa_angler.mk
+++ products/angler/pa_angler.mk
@@ -28,7 +28,7 @@ PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := huawei
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=angler \
-	BUILD_FINGERPRINT="google/angler/angler:6.0/MDB08M/2353240:user/release-keys" \
-	PRIVATE_BUILD_DESC="angler-user 6.0 MDB08M 2353240 release-keys"
+	BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
+	PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
 
 endif
