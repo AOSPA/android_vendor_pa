07d72c46426bbc1d189502fea90d38da96395c9d
Evan Anderson <evananderson@aospa.co>
2015-12-15 21:41:25 -0600

    angler: Capitalize Huawei
    Change-Id: Idbf132e763dbc1ae50eaa2a48cebb9d48abf5b1a
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- products/angler/pa_angler.mk
+++ products/angler/pa_angler.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 PRODUCT_NAME := pa_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
-PRODUCT_MANUFACTURER := huawei
+PRODUCT_MANUFACTURER := Huawei
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=angler \
 	BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
