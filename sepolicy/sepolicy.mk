5a54538b741c9f4615aa65a8a3b7dd8c8e616ca6
Jake Weinstein <xboxlover360@gmail.com>
2015-12-22 17:18:42 +0000

    vendor: BOARD_SEPOLICY_UNION is deprecated

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -5,18 +5,3 @@
 
 BOARD_SEPOLICY_DIRS += \
     vendor/pa/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    app.te \
-    bootanim.te \
-    drmserver.te \
-    file.te \
-    file_contexts \
-    mediaserver.te \
-    genfs_contexts \
-    installd.te \
-    seapp_contexts \
-    service_contexts \
-    system.te \
-    zygote.te \
-    vold.te
