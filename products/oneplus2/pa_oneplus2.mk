4c5a3f2f7cb82768dce4f1e064e466ecb8a84486
thecrazyskull <anaskarbila@aospa.co>
2016-02-29 11:35:58 -0500

    oneplus2: inherit 64bit configs directly from vendor
    Change-Id: Ibe4f16a5310d00e47e4cce484ef11c94d4ee24a4

--- products/oneplus2/pa_oneplus2.mk
+++ products/oneplus2/pa_oneplus2.mk
@@ -16,6 +16,7 @@
 ifeq (pa_oneplus2,$(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Include ParanoidAndroid common configuration
