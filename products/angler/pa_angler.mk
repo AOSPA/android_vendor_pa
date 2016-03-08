4559be98167df390806ba6569cbe983088d1933f
robbyf <rob_l_f_2000@hotmail.com>
2016-03-07 23:18:09 -0500

    Angler: Update Fingerprint
    Change-Id: Ieb37bdb612358bac9b7d641027048266297b1c88

--- products/angler/pa_angler.mk
+++ products/angler/pa_angler.mk
@@ -29,7 +29,6 @@ PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=angler \
-	BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29Q/2480792:user/release-keys \
-	PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29Q 2480792 release-keys"
-
+	BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29V/2554798:user/release-keys \
+	PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29V 2554798 release-keys"
 endif
