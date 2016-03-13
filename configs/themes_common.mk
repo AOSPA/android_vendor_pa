e59101892546aca9f0a728fe444c9725ced2eba9
d34d <clark@cyngn.com>
2016-03-13 02:55:11 +0100

    Themes: Refactor themes to vendor/theme [3/5]
    Change-Id: I6abea6ead1eb1980ec25f4184996cb234de21788

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -2,7 +2,9 @@
 PRODUCT_PACKAGES += \
     aapt \
     ThemeChooser \
-    ThemesProvider
+    ThemesProvider \
+    cm.theme.platform-res \
+    cm.theme.platform
 
 PRODUCT_COPY_FILES += \
    vendor/pa/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
