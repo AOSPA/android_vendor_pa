6bc6000682f5614463f78d3c388f83bde677deaf
Rob Farnham <rob.farnham@aospa.co>
2016-05-06 17:50:01 -0400

    Angler: Update fingerprints MTC19T
    Change-Id: I4252656e849f2cf58c625ff43df346442a44dbdf

--- products/angler/pa_angler.mk
+++ products/angler/pa_angler.mk
@@ -29,6 +29,6 @@ PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=angler \
-	BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19Q/2705526:user/release-keys \
-	PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19Q 2705526 release-keys"
+	BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19T/2741993:user/release-keys \
+	PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19T 2741993 release-keys"
 endif
