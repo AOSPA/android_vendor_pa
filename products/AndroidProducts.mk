698ebae64143e66e24985a395711822e7bc6ee30
Alexis Rico <me@sferadev.com>
2016-07-08 10:29:16 -0400

    vendor: Remove caf devices from marshmallow branch
    Change-Id: Ieb9a081388d496bf75e339fb586ea39833b10bd0

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -107,18 +107,6 @@ ifeq (pa_karin_windy,$(TARGET_PRODUCT))
 endif
 
 # CAF
-ifeq (pa_bacon,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/bacon/pa_bacon.mk
-endif
 ifeq (pa_dragon,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/dragon/pa_dragon.mk
-endif
-ifeq (pa_oneplus2,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk
-endif
-ifeq (pa_onyx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/onyx/pa_onyx.mk
-endif
-ifeq (pa_trltexx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/trltexx/pa_trltexx.mk
 endif
