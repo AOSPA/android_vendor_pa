42f62de98f8bcfb955a6c6f0b1f58f0e90c49411
Alex Naidis <alex.naidis@linux.com>
2016-07-26 17:25:50 +0200

    products: robin: update Fingerprint to the coming OTA
    Change-Id: Ic965f4ea193d12b3c892e4b0473635d53fd655ef
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- products/robin/pa_robin.mk
+++ products/robin/pa_robin.mk
@@ -36,7 +36,7 @@ PRODUCT_MODEL := Robin
 PRODUCT_MANUFACTURER := Nextbit
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-     BUILD_FINGERPRINT=Nextbit/ether/ether:6.0.1/MMB29M/00WW_1_290:user/release-keys \
-     PRIVATE_BUILD_DESC="ether-user 6.0.1 MMB29M 00WW_1_290 release-keys"
+     BUILD_FINGERPRINT=Nextbit/ether/ether:6.0.1/MMB29M/00WW_1_370:user/release-keys \
+     PRIVATE_BUILD_DESC="ether-user 6.0.1 MMB29M 00WW_1_370 release-keys"
 
 endif
