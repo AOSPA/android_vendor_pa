e29f6e55bf55d40142878f3518a483a274cb2702
Ícaro Hoff <icarohoff@gmail.com>
2016-07-27 09:44:07 -0300

    pa: Add support for the remaining LG G2 variants
    Devices:
    D800 (AT&T) -> https://github.com/GalaticStryder/device_lge_d800
    D801 (T-Mobile) -> https://github.com/GalaticStryder/device_lge_d801
    LS980 (Sprint) -> https://github.com/GalaticStryder/device_lge_ls980
    VS980 (Verizon) -> https://github.com/GalaticStryder/device_lge_vs980
    
    Change-Id: I923ffd76066fb8a6de4799f8ca48d5d0dc03f871
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 ParanoidAndroid Project
+# Copyright (C) 2016 - The Paranoid Android Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 
 # CAF
-    PRODUCT_MAKEFILES += \
+PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/bacon/pa_bacon.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
@@ -22,15 +22,18 @@
     $(LOCAL_DIR)/robin/pa_robin.mk
 
 # Samsung Exynos
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/gts210ltexx/pa_gts210ltexx.mk
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/gts210ltexx/pa_gts210ltexx.mk
 
 # LG G Platform
-    PRODUCT_MAKEFILES += \
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/d800/pa_d800.mk \
+    $(LOCAL_DIR)/d801/pa_d801.mk \
     $(LOCAL_DIR)/d802/pa_d802.mk \
     $(LOCAL_DIR)/d850/pa_d850.mk \
     $(LOCAL_DIR)/d851/pa_d851.mk \
     $(LOCAL_DIR)/d852/pa_d852.mk \
     $(LOCAL_DIR)/d855/pa_d855.mk \
+    $(LOCAL_DIR)/ls980/pa_ls980.mk \
     $(LOCAL_DIR)/ls990/pa_ls990.mk \
+    $(LOCAL_DIR)/vs980/pa_vs980.mk \
     $(LOCAL_DIR)/vs985/pa_vs985.mk
-
