550d878c84cee6b42ddf5f1881aa71870dc0c79e
Ícaro Hoff <icarohoff@gmail.com>
2016-07-27 09:44:07 -0300

    pa: Add support for the remaining LG G2 variants
    Devices:
    D800 (AT&T) -> https://github.com/GalaticStryder/device_lge_d800
    D801 (T-Mobile) -> https://github.com/GalaticStryder/device_lge_d801
    D803 (Rogers) -> https://github.com/GalaticStryder/device_lge_d803
    LS980 (Sprint) -> https://github.com/GalaticStryder/device_lge_ls980
    VS980 (Verizon) -> https://github.com/GalaticStryder/device_lge_vs980
    
    Change-Id: I923ffd76066fb8a6de4799f8ca48d5d0dc03f871
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

---
+++ products/vs980/pa_vs980.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2016 - The ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_vs980,$(TARGET_PRODUCT))
+
+# Inherit the new AOSP telephony configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include ParanoidAndroid common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+include vendor/pa/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/lge/vs980/vs980.mk)
+
+# Override build properties
+PRODUCT_DEVICE := vs980
+PRODUCT_NAME := pa_vs980
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-VS980
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_FINGERPRINT=lge/g2_vzw/g2:5.0.2/LRX22G/15046001715f9:user/release-keys \
+	PRIVATE_BUILD_DESC="g2_vzw-user 5.0.2 LRX22G 15046001715f9 release-keys"
+
+endif
