ff9dbcf14993853aaa45dde0c440eb74dc7870ca
Ícaro Hoff <icarohoff@gmail.com>
2016-07-27 09:44:07 -0300

    DO NOT MERGE: products: add support for the remaining LG G2 variants
    Only D802 is tested, other variants will be built/shared/tested and them
    'DO NOT MERGE' will be removed.
    
    The international variant (D802/5/6) targets the majority of the users.
    
    Support will be extended gradually to the following variants:
    
    - D800 (AT&T) -> https://github.com/GalaticStryder/device_lge_d800
    - D801 (T-Mobile) -> https://github.com/GalaticStryder/device_lge_d801
    - D803 (Rogers) -> https://github.com/GalaticStryder/device_lge_d803
    - LS980 (Sprint) -> https://github.com/GalaticStryder/device_lge_ls980
    - VS980 (Verizon) -> https://github.com/GalaticStryder/device_lge_vs980
    
    Change-Id: I923ffd76066fb8a6de4799f8ca48d5d0dc03f871
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

---
+++ products/d801/pa_d801.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2016 - The ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_d801,$(TARGET_PRODUCT))
+
+# Inherit the new AOSP telephony configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include ParanoidAndroid common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+include vendor/pa/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d801/d801.mk)
+
+# Override build properties
+PRODUCT_DEVICE := d801
+PRODUCT_NAME := pa_d801
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D801
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_FINGERPRINT=lge/g2_tmo_us/g2:5.0.2/LRX22G/1507914346dda:user/release-keys \
+	PRIVATE_BUILD_DESC="g2_tmo_us-user 5.0.2 LRX22G 1507914346dda release-keys"
+
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,12 @@ add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_gts210ltexx-userdebug
 
 # LGE
+add_lunch_combo pa_d800-userdebug
+add_lunch_combo pa_d801-userdebug
 add_lunch_combo pa_d802-userdebug
+add_lunch_combo pa_d803-userdebug
+add_lunch_combo pa_ls980-userdebug
+add_lunch_combo pa_vs980-userdebug
 
 # AOSP
 add_lunch_combo pa_angler-userdebug
