00b7fb904fab35c32b70f05cfeeb367a1746b794
Alex Naidis <alex.naidis@linux.com>
2016-08-05 21:22:57 +0200

    main: move dash blocker to qc/common
    This reverts commit 464d01f712bdee87151b3ebb40025c9b3ef3e001.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- main.mk
+++ main.mk
@@ -83,10 +83,6 @@ PRODUCT_PACKAGES += PA_Browser
 # Build ParanoidHub
 PRODUCT_PACKAGES += ParanoidHub
 
-# Block mm-dash by default
-
-TARGET_DISABLE_DASH ?= true
-
 # Include the custom PA bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
      PRODUCT_COPY_FILES += vendor/pa/prebuilt/bootanimation/720.zip:system/media/bootanimation.zip
