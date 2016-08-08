b4528c57fa8508a68fc25a8f59adc784dc1f8643
Evan Anderson <evananderson@aospa.co>
2016-08-07 19:59:04 -0500

    products: Update for August 5th Security
    Change-Id: Ib8b44a455091f83ce7547b9970a1f318e37fba0f
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- products/flo/pa_flo.mk
+++ products/flo/pa_flo.mk
@@ -29,7 +29,7 @@ PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=razor \
-	BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30P/2960889:user/release-keys \
-	PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30P 2960889 release-keys"
+	BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
+	PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
 
 endif
