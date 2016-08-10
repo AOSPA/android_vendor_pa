13f11a106a67d7f89d6f10dc81834f8ca7262938
Ícaro Hoff <icarohoff@gmail.com>
2016-08-10 10:40:47 -0300

    pa: consolidate makefiles across all LG family
    Regardless of the maintainer, they shouldn't diverge so much.
    
    - Updated headers to match "Paranoid Android"
    - Updated makefile descriptors universally
    - D855/VS985 were a bit messed up
    
    Change-Id: I07641fb55b2e51f62c03c949909d819c344c3836
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- products/vs985/pa_vs985.mk
+++ products/vs985/pa_vs985.mk
@@ -1,28 +1,44 @@
+# Copyright (C) 2016 The Paranoid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Check for target product
 ifeq (pa_vs985,$(TARGET_PRODUCT))
 
-# Inherit from the common Open Source product configuration
+# Inherit the AOSP telephony configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Include ParanoidAndroid common configuration
+# Include the common makefile for QCOM devices
+include device/qcom/common/common.mk
+
+# Include PA common configuration
 TARGET_BOOT_ANIMATION_RES := 1440
 include vendor/pa/main.mk
 
-$(call inherit-product, device/lge/vs985/full_vs985.mk)
+# Inherit device configuration
+$(call inherit-product, device/lge/vs985/vs985.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_NAME := pa_vs985
+# Override device properties
 PRODUCT_DEVICE := vs985
+PRODUCT_NAME := pa_vs985
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS985
-PRODUCT_MANUFACTURER := lge
-
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_vzw_us" \
     BUILD_FINGERPRINT="lge/g3_vzw/g3:6.0/MRA58K/160141503d43c:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_vzw-user 6.0 MRA58K 160141503d43c release-keys"
-
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 endif
