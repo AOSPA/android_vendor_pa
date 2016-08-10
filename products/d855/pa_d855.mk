e7107ebe33263fb891fec6a19428925768a905e0
Ícaro Hoff <icarohoff@gmail.com>
2016-08-10 10:40:47 -0300

    pa: consolidate makefiles across all LG family
    Regardless of the maintainer, they shouldn't diverge so much.
    
    - Updated headers to match "Paranoid Android"
    - Updated makefile descriptors universally
    - D855/VS985 were a bit messed up
    - Added frameworks/av for MSM8974
    
    Change-Id: I07641fb55b2e51f62c03c949909d819c344c3836
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- products/d855/pa_d855.mk
+++ products/d855/pa_d855.mk
@@ -1,20 +1,39 @@
+# Copyright (C) 2016 The Paranoid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Check for target product
 ifeq (pa_d855,$(TARGET_PRODUCT))
 
-# Include ParanoidAndroid common configuration
+# Inherit the AOSP telephony configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include the common makefile for QCOM devices
+include device/qcom/common/common.mk
+
+# Include PA common configuration
 TARGET_BOOT_ANIMATION_RES := 1440
 include vendor/pa/main.mk
 
-$(call inherit-product, device/lge/d855/full_d855.mk)
+# Inherit device configuration
+$(call inherit-product, device/lge/d855/d855.mk)
 
-# Inherit some common Turbo stuff.
-#$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
-
-# Enhanced NFC
-#$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
-
+# Override device properties
 PRODUCT_NAME := pa_d855
-
+PRODUCT_DEVICE := d855
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D855
+PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
