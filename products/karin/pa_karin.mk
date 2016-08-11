5ecc861030d6ff6b5fd2b4294ed86a451ed1bf3c
Tim Graham <tomgus1.2@gmail.com>
2016-08-11 02:58:52 -0400

    pa: updated dependencies for rhine and kitakami
    Change-Id: Idf1351214b3395c70957d082cc8b57c6217f9e30

--- products/karin/pa_karin.mk
+++ products/karin/pa_karin.mk
@@ -20,8 +20,6 @@ include vendor/pa/main.mk
 
 # Inherit AOSP device configuration
 $(call inherit-product, device/sony/karin/aosp_sgp771.mk)
-$(call inherit-product, device/sony/common/common_pa.mk)
-include device/sony/common/CommonConfigPA.mk
 
 # Override AOSP build properties
 PRODUCT_NAME := pa_karin
