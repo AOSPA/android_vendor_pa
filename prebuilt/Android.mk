ff442f30513e8ead34250bc3fe6bc874c73f1128
Alex Naidis <alex.naidis@linux.com>
2016-08-12 22:56:23 +0200

    pa: some more hax
    Change-Id: Iaa182dcaa17f7b4045b70ca4ffe81a05b6210d21
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -5,12 +5,11 @@ LOCAL_MODULE        := libjni_latinimegoogle
 LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_STRIP_MODULE  := false
-LOCAL_MULTILIB      := 64
+LOCAL_MULTILIB      := both
+LOCAL_OVERRIDES_PACKAGES := libjni_latinime
 LOCAL_MODULE_TAGS   := optional
-LOCAL_SRC_FILES_arm64  := lib64/libjni_latinimegoogle.so
-LOCAL_MODULE_PATH_arm64   := $(PRODUCT_OUT)/system/lib64
+LOCAL_SRC_FILES_32  := lib/libjni_latinimegoogle.so
+LOCAL_SRC_FILES_64  := lib64/libjni_latinimegoogle.so
+LOCAL_MODULE_PATH_32   := $(PRODUCT_OUT)/system/lib
+LOCAL_MODULE_PATH_64   := $(PRODUCT_OUT)/system/lib64
 include $(BUILD_PREBUILT)
-
-$(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE) | $(ACP)
-	@echo "Install (overridden): $@"
-	$(hide) $(ACP) -fp $< $(dir $@)libjni_latinime.so
