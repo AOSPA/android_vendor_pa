7697a4e86d6186b221bbb3c95887cae6eabb84b5
Evan Anderson <evananderson@aospa.co>
2016-08-28 17:20:41 -0400

    AndroidProducts: Drop conditionals
    There is no need to conditional include device makefiles
    because the build system automatically grabs the correct one
    for the product that is being built. This also fixes parsing
    errors by the new build system
    
    Change-Id: I0625a2810b8a0b2c86d1afc5c0cb14a9d5effd2f
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -12,48 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 # CAF
-ifeq (pa_bacon,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/bacon/pa_bacon.mk
-endif
-ifeq (pa_oneplus2,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk
-endif
-ifeq (pa_oneplus3,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk
-endif
-ifeq (pa_onyx,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/onyx/pa_onyx.mk
-endif
-ifeq (pa_robin,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/robin/pa_robin.mk
-endif
+    PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/bacon/pa_bacon.mk \
+    $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
+    $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
+    $(LOCAL_DIR)/onyx/pa_onyx.mk \
+    $(LOCAL_DIR)/robin/pa_robin.mk
 
 # Samsung Exynos
-ifeq (pa_gts210ltexx,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/gts210ltexx/pa_gts210ltexx.mk
-endif
 
 # LG G Platform
-ifeq (pa_d802,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/d802/pa_d802.mk
-endif
-ifeq (pa_d850,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/d850/pa_d850.mk
-endif
-ifeq (pa_d851,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/d851/pa_d851.mk
-endif
-ifeq (pa_d852,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/d852/pa_d852.mk
-endif
-ifeq (pa_d855,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/d855/pa_d855.mk
-endif
-ifeq (pa_ls990,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/ls990/pa_ls990.mk
-endif
-ifeq (pa_vs985,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/vs985/pa_vs985.mk
-endif
+    PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/d802/pa_d802.mk \
+    $(LOCAL_DIR)/d850/pa_d850.mk \
+    $(LOCAL_DIR)/d851/pa_d851.mk \
+    $(LOCAL_DIR)/d852/pa_d852.mk \
+    $(LOCAL_DIR)/d855/pa_d855.mk \
+    $(LOCAL_DIR)/ls990/pa_ls990.mk \
+    $(LOCAL_DIR)/vs985/pa_vs985.mk
 
