57541b9c251f9b72cbaf9cd3735de0e8a2e121a0
thecrazyskull <anaskarbila@aospa.co>
2016-09-18 11:18:05 -0400

    vendor: build snapdragon apps
    Change-Id: I418071d92e6e63dcb63148f85959814661bc7ca1

--- main.mk
+++ main.mk
@@ -78,6 +78,12 @@ PRODUCT_COPY_FILES += \
 # Build Chromium for Snapdragon (PA Browser)
 PRODUCT_PACKAGES += PA_Browser
 
+# Build Snapdragon apps
+PRODUCT_PACKAGES += \
+    SnapdragonCamera \
+    SnapdragonGallery \
+    SnapdragonMusic
+
 # Build ParanoidHub
 PRODUCT_PACKAGES += ParanoidHub
 
