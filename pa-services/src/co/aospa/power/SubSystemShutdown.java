900d3a360b7a7f67c4ae1d40459825f63491f446
Alex Naidis <alex.naidis@linux.com>
2016-09-30 15:37:27 +0200

    pa: Introduce pa-services
    Oriented by oem-services from Qualcomm.
    
    This is our easily extendable OEM hook which is
    currently called while shutting down.
    If present in calls into the Qualcomm
    SubsystemShutdown library in order
    to perform a proper modem shutdown.
    
    Ticket: NOUGAT-13
    
    Change-Id: If060459b9f45096ee2ccf6895d74515cead2246e
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

---
+++ pa-services/src/co/aospa/power/SubSystemShutdown.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 Paranoid Android
+ *
+ */
+
+package co.aospa.power;
+
+public final class SubSystemShutdown {
+    private static final String TAG = "SubSystemShutdown";
+
+    static {
+
+    try {
+        // Call into the Qualcomm SubSystemShutdown Library
+        System.loadLibrary(TAG);
+    } catch (UnsatisfiedLinkError e) {
+        System.err.println("Failed to load Qualcomm SubSystemShutdown library\n" + e);
+    }
+
+    }
+    public static native int shutdown();
+}
+
