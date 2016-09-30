b99735eac8d83eade95c8fe5a9613185ea6c4c8d
Alex Naidis <alex.naidis@linux.com>
2016-09-30 15:37:27 +0200

    pa: Introduce pa-services
    Oriented by oem-services from Qualcomm.
    
    This is our easily extendable OEM hook which is
    currently called while shutting down.
    
    Ticket: NOUGAT-1
    
    Change-Id: If060459b9f45096ee2ccf6895d74515cead2246e
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

---
+++ pa-services/src/co/aospa/power/ShutdownAOSPA.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 Paranoid Android
+ *
+ */
+
+package co.aospa.power;
+
+import android.util.Log;
+
+public final class ShutdownAOSPA {
+    private static final String TAG = "AOSPAShutdown";
+
+    public void rebootOrShutdown(boolean isreboot, String rebootreason) {
+        Log.i(TAG, "Triggered!");
+        if (SubSystemShutdown.shutdown() != 0) {
+            Log.e(TAG, "Failed to shutdown modem.");
+        }
+        else {
+            Log.i(TAG, "Modem shutdown successful.");
+        }
+    }
+}
+
