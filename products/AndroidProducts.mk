27cf88c736b4afa39c7c97fdbc01ae2fef23a803
Tim Graham <tomgus1.2@gmail.com>
2016-10-24 10:32:02 -0400

    products: Initial additions of Sony to CAF
    Change-Id: I8fbed2227cae700a941f7c3b34f09b5522fabc20

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -34,3 +34,33 @@
     $(LOCAL_DIR)/ls990/pa_ls990.mk \
     $(LOCAL_DIR)/vs985/pa_vs985.mk
 
+# Sony Yukon platform
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/flamingo/pa_flamingo.mk \
+                                          $(LOCAL_DIR)/eagle/pa_eagle.mk \
+                                          $(LOCAL_DIR)/tianchi/pa_tianchi.mk \
+                                          $(LOCAL_DIR)/seagull/pa_seagull.mk
+
+# Sony Rhine platform
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/honami/pa_honami.mk \
+                                          $(LOCAL_DIR)/amami/pa_amami.mk \
+                                          $(LOCAL_DIR)/togari/pa_togari.mk
+
+# Sony Shinano platform
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/aries/pa_aries.mk \
+                                          $(LOCAL_DIR)/sirius/pa_sirius.mk \
+                                          $(LOCAL_DIR)/castor/pa_castor.mk \
+                                          $(LOCAL_DIR)/castor_windy/pa_castor_windy.mk \
+                                          $(LOCAL_DIR)/leo/pa_leo.mk \
+                                          $(LOCAL_DIR)/scorpion/pa_scorpion.mk \
+                                          $(LOCAL_DIR)/scorpion_windy/pa_scorpion_windy.mk
+
+# Sony Kitakami platform
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/ivy/pa_ivy.mk \
+                                          $(LOCAL_DIR)/karin/pa_karin.mk \
+                                          $(LOCAL_DIR)/karin_windy/pa_karin_windy.mk \
+                                          $(LOCAL_DIR)/sumire/pa_sumire.mk \
+                                          $(LOCAL_DIR)/suzuran/pa_suzuran.mk \
+                                          $(LOCAL_DIR)/satsuki/pa_satsuki.mk
+
+# Sony Loire platform
+PRODUCT_MAKEFILES += $(LOCAL_DIR)/suzu/pa_suzu.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,3 +19,33 @@ add_lunch_combo pa_d852-userdebug
 add_lunch_combo pa_d855-userdebug
 add_lunch_combo pa_ls990-userdebug
 add_lunch_combo pa_vs985-userdebug
+
+# Sony Yukon Platform
+add_lunch_combo pa_eagle-userdebug
+add_lunch_combo pa_flamingo-userdebug
+add_lunch_combo pa_seagull-userdebug
+add_lunch_combo pa_tianchi-userdebug
+
+# Sony Rhine Platform
+add_lunch_combo pa_amami-userdebug
+add_lunch_combo pa_honami-userdebug
+add_lunch_combo pa_togari-userdebug
+
+# Sony Shinano Platform
+add_lunch_combo pa_aries-userdebug
+add_lunch_combo pa_castor-userdebug
+add_lunch_combo pa_castor_windy-userdebug
+add_lunch_combo pa_leo-userdebug
+add_lunch_combo pa_scorpion-userdebug
+add_lunch_combo pa_scorpion_windy-userdebug
+add_lunch_combo pa_sirius-userdebug
+
+# Sony Kitakami Platform
+add_lunch_combo pa_ivy-userdebug
+add_lunch_combo pa_karin-userdebug
+add_lunch_combo pa_karin_windy-userdebug
+add_lunch_combo pa_sumire-userdebug
+add_lunch_combo pa_suzuran-userdebug
+
+# Sony Loire Platform
+add_lunch_combo pa_suzu-userdebug
