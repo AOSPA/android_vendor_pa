244194a5c14dfa936346b7dbe42a7b493bd2542b
thecrazyskull <anaskarbila@aospa.co>
2016-11-16 10:09:10 -0500

    vendor: remove unsupported targets
    Change-Id: Ie533c3351d37d2a3a5d5283f02bca04346c5c3b6

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,19 +3,8 @@ add_lunch_combo pa_bacon-userdebug
 add_lunch_combo pa_oneplus2-userdebug
 add_lunch_combo pa_oneplus3-userdebug
 add_lunch_combo pa_onyx-userdebug
-add_lunch_combo pa_r7plus-userdebug
 add_lunch_combo pa_trltexx-userdebug
-add_lunch_combo pa_titan-userdebug
 add_lunch_combo pa_robin-userdebug
 
 # Samsung Exynos
 add_lunch_combo pa_gts210ltexx-userdebug
-
-# LG G Platform
-add_lunch_combo pa_d802-userdebug
-add_lunch_combo pa_d850-userdebug
-add_lunch_combo pa_d851-userdebug
-add_lunch_combo pa_d852-userdebug
-add_lunch_combo pa_d855-userdebug
-add_lunch_combo pa_ls990-userdebug
-add_lunch_combo pa_vs985-userdebug
