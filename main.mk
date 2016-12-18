c9f4b7a018447443880100e682ea11da5b7748e5
Alex Naidis <alex.naidis@linux.com>
2016-12-18 21:13:29 +0100

    pa: Clear security patch level
    Ticket: NOUGAT-26
    
    Change-Id: Ia3a2d584c82ef67d9d15f8e4501fcca093cdea6a
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- main.mk
+++ main.mk
@@ -112,6 +112,9 @@ ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += vendor/pa/prebuilt/bootanimation/1440.zip:system/media/bootanimation.zip
 endif
 
+# Clear security patch level
+PLATFORM_SECURITY_PATCH :=
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
