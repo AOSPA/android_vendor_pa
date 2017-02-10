b292056a283a71d6a3b013b91b6099b1f3cfcde1
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2017-02-10 13:05:19 +0100

    pa: robin: update dependencies to nougat-mr2
    * Changed device configuration
    * Added nougat 8994(8992) HAL sources
    
    Change-Id: I138e64f2d88fbc7fea93e7c2785b51f8cb9b651d
    Signed-off-by: Hernán Castañón Álvarez <hernanderes34@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,9 +5,9 @@ add_lunch_combo pa_oneplus2-userdebug
 add_lunch_combo pa_oneplus3-userdebug
 add_lunch_combo pa_onyx-userdebug
 add_lunch_combo pa_r7plus-userdebug
+add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_trltexx-userdebug
 add_lunch_combo pa_titan-userdebug
-add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_zl1-userdebug
 
 # Samsung Exynos devices
