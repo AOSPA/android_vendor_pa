3e0bf1d013f22ee2ba56157b61dd799657d0b5c9
Ícaro Hoff <icarohoff@gmail.com>
2017-03-25 19:23:53 -0300

    products: add support for the LeEco Le Pro3 (caf)
    Purely CAF codebase, OEM device naming avoided to switch pro3/max2 as
    they reveal the device more properly than zl1/x2 or something useless
    and meaningless like that.
    
    Change-Id: If229ba2cb1f25abaf3971d48de566fe100032a1c
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -19,7 +19,8 @@
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
-    $(LOCAL_DIR)/robin/pa_robin.mk
+    $(LOCAL_DIR)/robin/pa_robin.mk \
+    $(LOCAL_DIR)/pro3/pa_pro3.mk
 
 # Samsung Exynos
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/gts210ltexx/pa_gts210ltexx.mk
