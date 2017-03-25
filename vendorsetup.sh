161254eb66fae94bd983bffd53ce5c06ff9c283f
Ícaro Hoff <icarohoff@gmail.com>
2017-03-25 19:23:53 -0300

    products: add support for zl1 target (LeEco Le Pro3)
    Uses CAF HALs and blobs.
    
    PS1: Switch to ZL1 naming to inline with OEM.
    PS2: Remove overrides already set by init.
    PS3: Add Kernel, device tree and vendor (AOSPA).
    
    Change-Id: If229ba2cb1f25abaf3971d48de566fe100032a1c
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,7 @@ add_lunch_combo pa_r7plus-userdebug
 add_lunch_combo pa_trltexx-userdebug
 add_lunch_combo pa_titan-userdebug
 add_lunch_combo pa_robin-userdebug
+add_lunch_combo pa_zl1-userdebug
 
 # Samsung Exynos devices
 add_lunch_combo pa_gts210ltexx-userdebug
