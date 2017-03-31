05facb2a0545189a60ac015b68261289e844f9d8
Alex Naidis <alex.naidis@linux.com>
2017-03-31 20:17:42 +0200

    pa: oneplus2: Rebringup for nougat-mr2
    Change-Id: Ieafb12e98118ef9c48187e6b61c84486895c9d21
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- products/oneplus2/pa_oneplus2.mk
+++ products/oneplus2/pa_oneplus2.mk
@@ -21,19 +21,31 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Advanced platform features
+TARGET_DISABLE_DASH := false
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# Use AOSP camera application.
+TARGET_USES_AOSP_CAMERA := true
+
 include device/qcom/common/common.mk
 
 # Inherit AOSP device configuration
-$(call inherit-product, device/oneplus/oneplus2/device.mk)
+$(call inherit-product, device/oneplus/oneplus2/oneplus2.mk)
 
 # Override AOSP build properties
 PRODUCT_NAME := pa_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := ONE A2003
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447840920:user/release-keys \
+    PRIVATE_BUILD_DESC="OnePlus2-user 6.0.1 MMB29M 20 dev-keys"
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
 
 # Paranoid Android platform
 include vendor/pa/main.mk
