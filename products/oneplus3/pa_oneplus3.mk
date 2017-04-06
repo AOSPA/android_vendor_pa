3071fda35e67abe991148a2d1bec9eed7fbff464
Alex Naidis <alex.naidis@linux.com>
2017-04-06 19:56:07 +0200

    pa: oneplus3: Update device variable configuration
    * Update build fingerprint
    * Clear device variable, to switch it
      to handling via init extension.
    
    Change-Id: I16d21743c03cab2d3a2a408b3dab30406852c6c7
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- products/oneplus3/pa_oneplus3.mk
+++ products/oneplus3/pa_oneplus3.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 ParanoidAndroid Project
+# Copyright (C) 2016-2017 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,14 +36,14 @@ include vendor/pa/main.mk
 
 # Override AOSP build properties
 PRODUCT_NAME := pa_oneplus3
-PRODUCT_DEVICE := oneplus3
+PRODUCT_DEVICE :=
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-     BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:7.0/NRD90M/12161319:user/release-keys \
-     PRIVATE_BUILD_DESC="OnePlus3-user 7.0 NRD90M 9 dev-keys"
+     BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/03061309:user/release-keys \
+     PRIVATE_BUILD_DESC="OnePlus3-user 7.1.1 NMF26F 17 dev-keys"
 
 endif
