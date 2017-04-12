5566d17981c2c0e38e62caf52575aa20a3eeec97
Evan Anderson <evananderson@aospa.co>
2017-04-12 18:13:05 -0400

    prebuilt: Add PA Gapps config
    * We want to always keep some of our stock apps installed when the user
    installs gapps. This configuration keeps SnapdragonCamera,
    SnapdragonGallery, and PABrowser installed when gapps are installed
    
    Change-Id: I82bc05ad761a8cc0a9ca7c49bf7698f32eefbdf8
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- main.mk
+++ main.mk
@@ -84,6 +84,10 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/install/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/pa/prebuilt/addon.d/50-backuptool.sh:system/addon.d/50-backuptool.sh
 
+# Include PA GApps config
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/install/gapps-config.txt:install/gapps-config.txt
+
 # Build Chromium for Snapdragon (PA Browser)
 PRODUCT_PACKAGES += PABrowser
 
