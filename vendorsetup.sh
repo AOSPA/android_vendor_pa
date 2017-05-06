ecc1efcb7e097b63057316398511c29ab6acaae2
Anas Karbila <anaskarbila@gmail.com>
2017-05-07 04:02:48 +0900

    pa: Add Pixel support
    Change-Id: I33dc526f8b2c817ab91551fc3858bbfad8f13dc6
    Signed-off-by: lennart <lennart.1997@gmx.de>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,5 @@ add_lunch_combo pa_gts210wifi-userdebug
 # AOSP
 add_lunch_combo pa_angler-userdebug
 add_lunch_combo pa_bullhead-userdebug
+add_lunch_combo pa_marlin-userdebug
+add_lunch_combo pa_sailfish-userdebug
