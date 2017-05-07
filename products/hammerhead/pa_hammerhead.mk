324cfd697cef9f8cd54df86f5b6c820bc97f0900
Marco Marinho <marcomarinho12@gmail.com>
2017-05-07 22:52:17 +0530

    pa: products: Add hammerhead for caf
    * Base support for hammerhead using caf platform
    
    Change-Id: Icf46e575ef86af4fc66843c185d6c74f72c9ec8b
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

---
+++ products/hammerhead/pa_hammerhead.mk
@@ -0,0 +1,48 @@
+# Copyright 2016 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (pa_hammerhead,$(TARGET_PRODUCT))
+
+# Use AOSP Camera Application
+TARGET_USES_AOSP_CAMERA := true
+
+# We have our own sepolicy
+TARGET_EXCLUDE_QCOM_SEPOLICY := true
+
+# Inherit AOSP device configuration for hammerhead
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Include ParanoidAndroid BootAnimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Generic CAF packages
+include device/qcom/common/common.mk
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_hammerhead
+PRODUCT_DEVICE := hammerhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := hammerhead
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
+
+endif
