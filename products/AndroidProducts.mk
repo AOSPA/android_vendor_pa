7016e122cff62aaec71b556549a79514f294034e
Marco Marinho <marcomarinho12@gmail.com>
2017-05-07 22:52:17 +0530

    pa: products: Add hammerhead for caf
    * Base support for hammerhead using caf platform
    
    Change-Id: Icf46e575ef86af4fc66843c185d6c74f72c9ec8b
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -30,5 +30,6 @@
     PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/angler/pa_angler.mk \
     $(LOCAL_DIR)/bullhead/pa_bullhead.mk \
+    $(LOCAL_DIR)/hammerhead/pa_hammerhead.mk \
     $(LOCAL_DIR)/marlin/pa_marlin.mk \
     $(LOCAL_DIR)/sailfish/pa_sailfish.mk
