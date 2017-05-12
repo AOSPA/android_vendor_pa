e7615dae8dbf1db50f8d7167766ec55befa3392b
33bca <fabian.leutenegger@bluewin.ch>
2017-05-12 12:21:34 +0200

    pa: products: add support for the Xiaomi Mi 5 (caf)
    Change-Id: I3af2a86c7db841634a410d0dd39913c4c94819c9

---
+++ products/gemini/pa_gemini.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2017 - The Paranoid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for the target product.
+ifeq (pa_gemini,$(TARGET_PRODUCT))
+
+# Inherit from those products, most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Set bootanimation to 1080p display.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Most advanced platform features, first.
+TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+
+# Inherit from our common CAF device tree.
+include device/qcom/common/common.mk
+
+# Inherit the device configuration itself.
+$(call inherit-product, device/xiaomi/gemini/device.mk)
+
+# Inherit the main configuration from our vendor.
+include vendor/pa/main.mk
+
+# Override device build properties.
+PRODUCT_NAME := pa_gemini
+PRODUCT_DEVICE := gemini
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 5
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Google client ID.
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="gemini" \
+    PRODUCT_NAME="gemini" \
+    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/V8.2.1.0.NAACNEB:user/release-keys" \
+    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V8.2.1.0.NAACNEB release-keys"
+
+TARGET_VENDOR := Xiaomi
+endif
