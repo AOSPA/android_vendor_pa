3a6c214af358b145cf7bacb75e77993cad4b2cfa
33bca <fabian.leutenegger@bluewin.ch>
2017-05-12 12:21:34 +0200

    pa: products: add support for the Xiaomi Mi 5 (caf)
    Change-Id: I3af2a86c7db841634a410d0dd39913c4c94819c9

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,6 +16,7 @@
 # CAF
     PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/bacon/pa_bacon.mk \
+    $(LOCAL_DIR)/gemini/pa_gemini.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
