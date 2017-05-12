32ba24bb430b971a78642453ccc13db69d9940b1
33bca <fabian.leutenegger@bluewin.ch>
2017-05-12 12:21:34 +0200

    products: add support for the Xiaomi Mi 5 (caf)
    Change-Id: I3af2a86c7db841634a410d0dd39913c4c94819c9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,13 @@
 # CAF
 add_lunch_combo pa_bacon-userdebug
+add_lunch_combo pa_gemini-userdebug
 add_lunch_combo pa_oneplus2-userdebug
 add_lunch_combo pa_oneplus3-userdebug
 add_lunch_combo pa_onyx-userdebug
 add_lunch_combo pa_r7plus-userdebug
+add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_trltexx-userdebug
 add_lunch_combo pa_titan-userdebug
-add_lunch_combo pa_robin-userdebug
 
 # Samsung Exynos devices
 add_lunch_combo pa_gts210ltexx-userdebug
