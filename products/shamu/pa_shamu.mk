940cd3560b0ea79a2b6ab0efa6d5f70a0267d5a6
Lennart Wieboldt <lennart.1997@gmx.de>
2017-05-30 17:33:12 +0200

    pa: Add Shamu support
    Change-Id: I732b2ab997267c1b56ae778a39a1b3f659211543
    Signed-off-by: Lennart Wieboldt <lennart.1997@gmx.de>

---
+++ products/shamu/pa_shamu.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2017 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_shamu,$(TARGET_PRODUCT))
+
+# Use AOSP Camera Application
+TARGET_USES_AOSP_CAMERA := true
+
+# Use the AOSP stack
+TARGET_USES_AOSP := true
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+# Generic CAF packages
+include device/qcom/common/common.mk
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27C/3853306:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27C 3853306 release-keys"
+endif
