bbca5df07cb066ee8daaca8ddd0eb93f89214b08
Jan Dula <frantisheq@gmail.com>
2017-06-01 21:44:09 +0200

    products: add support for kingdom target (Lenovo Vibe Z2 Pro)
    use personal github repo until merge time comes
    
    patch audio and build:
    
    cd hardware/qcom/audio && \
    git fetch https://gerrit.aospa.co/AOSPA/android_hardware_qcom_audio refs/changes/97/2697/1 && \
    git cherry-pick FETCH_HEAD && \
    git fetch https://gerrit.aospa.co/AOSPA/android_hardware_qcom_audio refs/changes/98/2698/1 && \
    git cherry-pick FETCH_HEAD && \
    git fetch https://gerrit.aospa.co/AOSPA/android_hardware_qcom_audio refs/changes/99/2699/1 && \
    git cherry-pick FETCH_HEAD && \
    git fetch https://gerrit.aospa.co/AOSPA/android_hardware_qcom_audio refs/changes/00/2700/1 && \
    git cherry-pick FETCH_HEAD && \
    cd ../../../vendor/pa && \
    git fetch https://gerrit.aospa.co/AOSPA/android_vendor_pa refs/changes/01/2701/4 && \
    git cherry-pick FETCH_HEAD && \
    cd ../.. && \
    . build/envsetup.sh && \
    ./rom-build.sh kingdom
    
    Change-Id: Ib1091f2c42babbebd3afc0a14d518c222c90311d
    Signed-off-by: Jan Dula <frantisheq@gmail.com>

---
+++ products/kingdom/pa_kingdom.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2015 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_kingdom,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include ParanoidAndroid common configuration
+TARGET_BOOT_ANIMATION_RES := 1440
+
+include device/qcom/common/common.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/lenovo/kingdom/kingdom.mk)
+
+include vendor/pa/main.mk
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_kingdom
+PRODUCT_DEVICE := kingdom
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := K920
+PRODUCT_MANUFACTURER := Lenovo
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+# Set via init library
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.name
+
+endif
