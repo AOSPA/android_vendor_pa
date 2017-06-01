7871a95015bd0830e5af226ff9c58a86e5d42f87
Sai Vishal <thug@droidthug.me>
2017-06-01 13:13:43 +0530

    [DO NOT MERGE] products: bring in mido(Redmi Note 4) for some #ParanoidLove
    Just a note:
    	* MSM8953 and MSM8996 have unified development in CodeAurora Forums
    	* Will test the nougat-mr2-8996 branch for HALs and report back
    
    Change-Id: I8cf059c5af1502d66f9b0fb4e085dd011ae4717e

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -17,6 +17,7 @@
     PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/bacon/pa_bacon.mk \
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
+    $(LOCAL_DIR)/mido/pa_mido.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
