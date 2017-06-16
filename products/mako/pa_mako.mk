0268b9c2a4b76e1a83f60a2ad154af69852f3c75
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2017-06-16 09:06:43 +0200

    pa: products: add mako
    Welcome back Google Nexus 4
    
    ** This is a W.I.P for testing, not sure if it will be ever come to PA.
    
    Change-Id: I99cddd3ece473a92c79624568dc8e5635168b813
    Signed-off-by: Hernán Castañón Álvarez <hernanderes34@gmail.com>

---
+++ products/mako/pa_mako.mk
@@ -0,0 +1,53 @@
+# Copyright 2016-2017 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (pa_mako,$(TARGET_PRODUCT))
+
+# We have our own power HAL
+#TARGET_USES_DEVICE_SPECIFIC_POWERHAL := true
+#GET_USES_QCOM_MM_AUDIO := false
+
+# Use AOSP Camera Application
+TARGET_USES_AOSP_CAMERA := true
+
+# We have our own sepolicy
+#TARGET_EXCLUDE_QCOM_SEPOLICY := true
+
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Generic CAF packages
+#include device/qcom/common/common.mk
+
+# Get the defaults going.
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+# Disable QC partial update
+#PRODUCT_PROPERTY_OVERRIDES += \
+    persist.mdp.partialUpdate=false
+
+# Override AOSP defaults that we want changed.
+PRODUCT_NAME := pa_mako
+PRODUCT_DEVICE := mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48T 2237560 release-keys"
+
+endif
