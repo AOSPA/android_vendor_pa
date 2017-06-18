1026127c8ca87b0efd430af0c453877590d1fde0
Alex Naidis <alex.naidis@linux.com>
2017-06-18 16:01:40 +0200

    pa: Update properties for SetupWizard
    The properties we were setting are outdated.
    This replaces them with the updated ones that
    we find in recent GMS drops.
    
    Change-Id: Ia909f9dd7476993a26e3502a6c576310461ebce3
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- main.mk
+++ main.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.require_network=any \
+    ro.setupwizard.mode=OPTIONAL \
     ro.opa.eligible_device=true
 
 # Override old AOSP default sounds with newer Google stock ones
