cf8ed657914cf2fec1c2e36f9056b94a6983cd55
Lennart Wieboldt <lennart.1997@gmx.de>
2017-07-06 10:41:30 +0200

    pa: products: update pixel/nexus fingerprints to july security patch level
    * angler  	  : N2G48B
    * bullhead        : N2G47Z
    * marlin/sailfish : NJH47D
    * shamu           : N6F27H
    
    Change-Id: I282a605202402fe0b81bb304408975e5e0210403

--- products/shamu/pa_shamu.mk
+++ products/shamu/pa_shamu.mk
@@ -40,6 +40,6 @@ PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27E/3939304:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27E 3939304 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27H/4072753:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27H 4072753 release-keys
 endif
