8aefed5c3d884643ce70c5577a985a55c8c04d67
Willi Ye <williye97@gmail.com>
2017-07-11 16:24:42 +0200

    pa: build: Only use update-alternatives if it's available
    Change-Id: I797335ea732affc86c1ea80497c11ec20e10d39d

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ if [ -z "$THREADS" ]; then
 fi
 
 # Pick the best JDK 8, if more than one is installed
-if [ "$(update-alternatives --list javac | wc -l)" -gt 1 ]; then
+if [ $(which update-alternatives > /dev/null 2>&1; echo $?) == "0" ] && [ "$(update-alternatives --list javac | wc -l)" -gt 1 ]; then
         JDK_PATH=$(dirname $(update-alternatives --list javac | grep '\-8\-') | tail -n1)
         JRE_PATH=$(dirname $JDK_PATH/../jre/bin/java)
         export PATH=$JDK_PATH:$JRE_PATH:$PATH
