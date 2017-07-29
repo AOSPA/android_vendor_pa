ee153e57931d26c2114cd878e4a8cd185b8a6209
viscropst <bcct2010@live.cn>
2017-07-29 14:26:18 +0800

    Add something for libra
    Change-Id: I64b07021e2e23970dbfe792ee4c589db93b9d447

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,7 +21,8 @@
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
     $(LOCAL_DIR)/robin/pa_robin.mk \
-    $(LOCAL_DIR)/zl1/pa_zl1.mk
+    $(LOCAL_DIR)/zl1/pa_zl1.mk \
+    $(LOCAL_DIR)/libra/pa_libra.mk
 
 # Samsung Exynos family
     PRODUCT_MAKEFILES += \
