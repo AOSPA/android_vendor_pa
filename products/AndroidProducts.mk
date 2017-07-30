65d1159eaea6dfa9bb11cf1a2c626b0bbbe3a9ee
viscropst <bcct2010@live.cn>
2017-07-30 14:00:41 +0800

    pa:products:add Xiaomi Mi-4c(libra)
    * Add Device Confgurations for libra
    * Add Device dependencies for libra(with nougat-msm8994(msm8992) HALs)
    
    Change-Id: I695008a98960ae7aacefc97d611edb7c51d954d8

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -17,11 +17,12 @@
     PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/bacon/pa_bacon.mk \
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
+    $(LOCAL_DIR)/libra/pa_libra.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
     $(LOCAL_DIR)/robin/pa_robin.mk \
-    $(LOCAL_DIR)/zl1/pa_zl1.mk
+    $(LOCAL_DIR)/zl1/pa_zl1.mk 
 
 # Samsung Exynos family
     PRODUCT_MAKEFILES += \
