f90bbe5fb3be2fdb1ddc2a0a2775c02413f97c5f
Lennart Wieboldt <lennart.1997@gmx.de>
2017-08-09 16:35:24 +0200

    pa: products: update fingerprints to august security patch level
    * marlin/sailfish : NZH54D
    * shamu           : N8I11B
    
    Change-Id: I4d63a84caa562047957d0bb781fbbda412763acf
    Signed-off-by: Lennart Wieboldt <lennart.1997@gmx.de>

--- products/marlin/pa_marlin.mk
+++ products/marlin/pa_marlin.mk
@@ -40,8 +40,8 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NJH47D/4045516:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 7.1.2 NJH47D 4045516 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NZH54D/4146044:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 7.1.2 NZH54D 4146044 release-keys"
 
 # Paranoid Android platform
 include vendor/pa/main.mk
