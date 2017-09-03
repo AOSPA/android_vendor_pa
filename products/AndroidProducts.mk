778fd9a18979351af6d37a8398cc62ce8679f166
Rohan Taneja <rohantaneja63@gmail.com>
2017-09-03 11:01:47 +0530

    products: add z2_plus AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,7 +21,8 @@
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
     $(LOCAL_DIR)/robin/pa_robin.mk \
-    $(LOCAL_DIR)/zl1/pa_zl1.mk
+    $(LOCAL_DIR)/zl1/pa_zl1.mk \
+    $(LOCAL_DIR)/z2_plus/pa_z2_plus.mk
 
 # Samsung Exynos family
     PRODUCT_MAKEFILES += \
