683b5c2f43fef96aa20f7ed5e46880ef9227f7ad
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put pa variables in the "Pa" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Lineage structure
 pa_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Pa": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
