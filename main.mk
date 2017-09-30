7348a6bf0646bbf0c4e4e654d956e1377bb23d87
Alex Naidis <alex.naidis@linux.com>
2017-09-30 14:52:15 -0400

    pa: Initial 8.0 bringup
    - Update version number
    - Cleanup sepolicy, remove theming support
    - Iniital pass at overlays
    
    Change-Id: I933a9fadc9d81da9454c5e369e841fcc22629932
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- main.mk
+++ main.mk
@@ -16,7 +16,7 @@ export VENDOR := pa
 
 # Include versioning information
 # Format: Major.minor.maintenance(-TAG)
-export PA_VERSION := 7.3.1-EOL
+export PA_VERSION := 8.0.0-DEV
 
 export ROM_VERSION := $(PA_VERSION)-$(shell date -u +%Y%m%d)
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -170,9 +170,6 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth Audio (A2DP)
 PRODUCT_PACKAGES += libbthost_if
-
-# Substratum
-PRODUCT_PACKAGES += ThemeInterfacer
 
 # Include vendor SEPolicy changes
 include vendor/pa/sepolicy/sepolicy.mk
