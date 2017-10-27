b558886d81751491cec0992fdc1c1ba310173caa
Alex Cruz <alex@dirtyunicorns.com>
2017-10-26 23:03:07 -0400

    Update imports for preferences
    Change-Id: I3435a9214b12615f5198ac304943f1860d7eeb77

--- support/src/com/pa/support/preference/GlobalCheckBoxPreference.java
+++ support/src/com/pa/support/preference/GlobalCheckBoxPreference.java
@@ -17,7 +17,7 @@
 package com.pa.support.preference;
 
 import android.content.Context;
-import android.preference.CheckBoxPreference;
+import android.support.v7.preference.CheckBoxPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
