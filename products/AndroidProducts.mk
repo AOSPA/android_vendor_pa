eebadd47e865eaa3bf95ba17768c5882275dec1f
Hernán Castañón <herna@paranoidandroid.co>
2017-11-27 22:13:34 +0100

    vendor: pa: add support for the OnePlus 5 and 5T
    Modded to use with private repos.
    
    Change-Id: I1b0088bbac7e25088fd2f6f4fa004c792f05d496
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -19,6 +19,7 @@
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
+    $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
     $(LOCAL_DIR)/robin/pa_robin.mk \
     $(LOCAL_DIR)/zl1/pa_zl1.mk
