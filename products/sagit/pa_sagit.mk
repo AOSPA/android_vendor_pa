b71a11375551c8c9e70625d5f348e171e058b5d7
TheStrix <parthbhatia98@gmail.com>
2017-12-28 15:31:43 +0530

    Add sagit
    Change-Id: I7edc432269b22f867627030d8dc0859353ee31f6

---
+++ products/sagit/pa_sagit.mk
@@ -0,0 +1,50 @@
+# Copyright (C) 2018 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_sagit,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_AOSP := true
+TARGET_USES_QCOM_BSP := false
+
+# Advanced platform features
+TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+include device/qcom/common/common.mk
+include vendor/pa/main.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/xiaomi/sagit/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_sagit
+PRODUCT_DEVICE := sagit
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 6
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.5.8.0.OCAMIFA:user/release-keys \
+    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.5.8.0.OCAMIFA release-keys"
+endif
