e126e68720cd3a49e354082b4a289e01686e1393
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    aospa: Sort soong variables and configurations
    Change-Id: I4a191f8ce61123e30f780f8e39411a7723fb6197

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,6 +2,7 @@ _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
