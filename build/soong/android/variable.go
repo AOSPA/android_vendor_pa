1d5b8aab66c1d5f80fda461dfb8115a5c279dee9
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    aospa: Sort soong variables and configurations
    Change-Id: I4a191f8ce61123e30f780f8e39411a7723fb6197

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,12 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	Target_shim_libs struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Target_shim_libs  *string `json:",omitempty"`
 }
