4622f0568735e48e9af66d310d7c2162e5f56767
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    pa: soong: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Change-Id: Ic8722c42807429f2faa3546316c71c40533ce195

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,6 +1,10 @@
 package android
 type Product_variables struct {
+	Target_shim_libs struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
+	Target_shim_libs  *string `json:",omitempty"`
 }
