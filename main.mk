7bb2f23ad0a6b52056188daf1524f32b3465abc7
Park Ju Hyung <qkrwngud825@gmail.com>
2018-03-06 15:56:32 +0900

    pa: Enable AOPT for Substratum
    Change-Id: I5f28bcfd38f7cb1fb08bf0745b6e8caa113273a7
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- main.mk
+++ main.mk
@@ -41,6 +41,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Ariel.ogg \
     ro.config.ringtone=Titania.ogg
 
+# Enable AOPT for Substratum
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.substratum.aopt=true
+
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
