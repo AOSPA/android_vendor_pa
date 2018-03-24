6691504c227d4e1c8a334ce19cdc188e30a1727b
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 11:17:49 -0700

    soong: Add TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I925b137edeed9fa11bbe566002147b444328c661
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,13 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+    Uses_qti_camera_device struct {
+        Cppflags []string
+        Shared_libs []string
+    }
 }
 
 type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
+    Uses_qti_camera_device  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,6 +2,7 @@ _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Uses_qti_camera_device,                $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
