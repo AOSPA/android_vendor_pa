18c8f4564f599be9f49f470f037d2f533af12965
Lennart Wieboldt <lennart.1997@gmx.de>
2018-04-20 16:24:10 +0200

    pa: move sdclang config files to vendor/pa (2/2)
    * Do not limit the usage of sdclang to devices which call
      the qcom/common repo
    
    Change-Id: I667e6270058d02fdd894b8242a6375b1a13637dd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,3 +30,8 @@ add_lunch_combo pa_suzu-userdebug
 add_lunch_combo pa_dora-userdebug
 add_lunch_combo pa_kagura-userdebug
 add_lunch_combo pa_keyaki-userdebug
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=vendor/pa/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false
