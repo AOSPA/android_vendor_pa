08d3d38ecb0cab45e91553e7e348a19c9a3a35ca
Tim <tomgus1.2@gmail.com>
2018-08-01 01:32:23 +0200

    pa: Added Xperia Yoshino Platform
    Added Yoshino devices to ParanoidAndroid. All three are ready for release and run well.
    
    Change-Id: Ib79bb3085236e7804d11d2effb578206463ad329

---
+++ products/maple/pa_maple.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2017 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_maple,$(TARGET_PRODUCT))
+
+# Use the AOSP stack
+TARGET_USES_AOSP := true
+TARGET_EXCLUDE_QCOM_SEPOLICY=true
+BOARD_USE_ENFORCING_SELINUX:= true
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/sony/maple/aosp_g8141.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_maple
+PRODUCT_MODEL := Xperia XZ Premium (AOSPA)
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+endif
