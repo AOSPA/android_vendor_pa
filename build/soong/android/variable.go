72bf580e4c64e8a0d3e9ebde8e49e92275554a2f
Danny Baumann <dannybaumann@web.de>
2018-08-29 11:04:58 +0200

    soong_config: Allow process-specific override of target SDK version
    Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+	Target_process_sdk_version_override struct {
+		Cppflags []string
+	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
@@ -10,5 +13,6 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,9 @@
 _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
+
 $(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str,  Target_process_sdk_version_override,   $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
