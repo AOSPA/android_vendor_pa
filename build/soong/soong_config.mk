cfca97d459320cc64d17b17dabc4df29b48f9baa
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:50:16 +0200

    Revert "soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS"
    * The per-process API level override will take care of this
    
    This reverts commit cf7f7d48d0297f9a64413076d755b00e10e48c9a.
    
    Change-Id: I3c0b5879ddf8e9dcc0b56a6e9a2855e515685389

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,6 @@
 _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
