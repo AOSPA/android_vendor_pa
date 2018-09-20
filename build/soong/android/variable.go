a810bf710877a43db57403adebfb9756485549cd
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:50:16 +0200

    Revert "soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS"
    * The per-process API level override will take care of this
    
    This reverts commit cf7f7d48d0297f9a64413076d755b00e10e48c9a.
    
    Change-Id: I3c0b5879ddf8e9dcc0b56a6e9a2855e515685389

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,14 +1,10 @@
 package android
 type Product_variables struct {
-	Needs_text_relocations struct {
-		Cppflags []string
-	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
-	Needs_text_relocations  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 }
