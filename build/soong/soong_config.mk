a0b7735cdb679371e15fee92da1f233314422eab
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:52:58 +0200

    pa: Use add_json_str_omitempty directive for TARGET_LD_SHIM_LIBS
    * The code is ifdef'ed out, so let's match that expecation here
    
    Change-Id: I59c8e276c3dd28c4f46222f66d626e48b2aa6551

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,7 +3,7 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_str,  Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Target_uses_color_metadata, $(filter true,$(TARGET_USES_COLOR_METADATA)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
