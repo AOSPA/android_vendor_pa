cc949370a2503bf3e1e3e48b7208acc107d4e30c
Adarsh-MR <adarshmr1998@gmail.com>
2018-09-21 14:37:04 +0000

    vendor/pa: soong: Add target_uses_color_metadata soong flag
    Change-Id: Id24a93d4e13fcdc596c1d9f40b50ddcd8a6cd8bb
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,6 +2,7 @@ _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_uses_color_metadata,            $(filter true,$(TARGET_USES_COLOR_METADATA)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
