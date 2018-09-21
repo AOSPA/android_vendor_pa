14155c5b8ad6654227393d3ca25fb4c134d7116e
Adarsh-MR <adarshmr1998@gmail.com>
2018-09-21 14:37:04 +0000

    vendor/pa: soong: Add target_uses_color_metadata soong flag
    Change-Id: Id24a93d4e13fcdc596c1d9f40b50ddcd8a6cd8bb
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,13 @@ type Product_variables struct {
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+	Target_uses_color_metadata struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Target_uses_color_metadata  *bool `json:",omitempty"`
 }
