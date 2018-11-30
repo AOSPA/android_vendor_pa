eada323e11b8cd7fa7601c7ddff83591ef57ef7c
Josh Choo <dev@joshchoo.com>
2018-11-30 17:47:41 +0000

    pa: msm8998
    Bringup CAF reference device.
    
    Change-Id: I17ebe95f779bc5f86b2b0b56a53a8bcf0515fa49
    Signed-off-by: Josh Choo <dev@joshchoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -31,6 +31,9 @@ add_lunch_combo pa_dora-userdebug
 add_lunch_combo pa_kagura-userdebug
 add_lunch_combo pa_keyaki-userdebug
 
+# CAF
+add_lunch_combo msm8998-userdebug
+
 # SDClang Environment Variables
 export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
 export SDCLANG_CONFIG=vendor/pa/sdclang/sdclang.json
