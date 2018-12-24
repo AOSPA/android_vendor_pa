849551c547bb7e005e401799476989fd4c2c6334
Offain <fabian.leutenegger@bluewin.ch>
2018-12-24 16:10:10 +0100

    pa: Add Xiaomi Mi Mix 2S
    Change-Id: I7a1aed2fdc13051e0c8bcf95026cabe0be4a6c6e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo pa_gemini-userdebug
 add_lunch_combo pa_oneplus2-userdebug
 add_lunch_combo pa_oneplus3-userdebug
 add_lunch_combo pa_onyx-userdebug
+add_lunch_combo pa_polaris-userdebug
 add_lunch_combo pa_r7plus-userdebug
 add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_trltexx-userdebug
