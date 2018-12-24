cf5d0d9a1d9dee1d4ba787205de03b21b4363911
Offain <fabian.leutenegger@bluewin.ch>
2018-12-24 16:10:10 +0100

    pa: Add Xiaomi Mi Mix 2S
    Change-Id: I7a1aed2fdc13051e0c8bcf95026cabe0be4a6c6e

---
+++ products/polaris/pa_polaris.mk
@@ -0,0 +1,54 @@
+# Copyright (C) 2018 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_polaris,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_AOSP := false
+TARGET_USES_QCOM_BSP := false
+
+# Advanced platform features
+#TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+include device/qcom/common/common.mk
+include vendor/pa/main.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/xiaomi/polaris/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_polaris
+PRODUCT_DEVICE := polaris
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi Mix 2S
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+
+BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="polaris-user 8.0.0 OPR1.170623.032 V9.5.19.0.ODGMIFA release-keys" \
+    PRODUCT_NAME="polaris" \
+    TARGET_DEVICE="polaris"
+
+endif
