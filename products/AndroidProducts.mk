0cfccd59d118a6ee46776c9170f61be2916976e6
Akash <akashniki@gmail.com>
2019-01-02 10:38:36 +0000

    vendor: add Oneplus6 and 6T under same name Oneplus6
    Change-Id: Ie232d3efe0a476124015712485a0a021f5a72920

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,6 +18,7 @@
     $(LOCAL_DIR)/bacon/pa_bacon.mk \
     $(LOCAL_DIR)/beryllium/pa_beryllium.mk \
     $(LOCAL_DIR)/dipper/pa_dipper.mk \
+    $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
