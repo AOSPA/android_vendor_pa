25cd58c960c935af31c864ff40849095ddf432e0
Akhil Narang <akhilnarang.1999@gmail.com>
2019-01-06 16:30:07 +0530

    pa: add whyred
    Change-Id: I095adea344101f231bb31a69af12c0ef19a92fdf
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ products/whyred/pa_whyred.mk
@@ -0,0 +1,58 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_whyred,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_AOSP := true
+TARGET_USES_QCOM_BSP := false
+
+# Advanced platform features
+#TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+include device/qcom/common/common.mk
+include vendor/pa/main.mk
+
+# Inherit from whyred device
+$(call inherit-product, device/xiaomi/whyred/device.mk)
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+PRODUCT_NAME := pa_whyred
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := whyred
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 5
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="whyred" \
+    PRODUCT_NAME="whyred" \
+    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+
+TARGET_VENDOR := Xiaomi
+endif
