cf4a85113df478b2b525c74cfac0e81426d03b89
spezi77 <spezi7713@gmx.net>
2019-01-28 22:35:50 +0100

    pa: marlin: Time for Pie!
    Change-Id: Ie2fb48a417b0ab873f3b44b865a0b28256f267fb
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/marlin/pa_marlin.mk
+++ products/marlin/pa_marlin.mk
@@ -34,11 +34,13 @@ PRODUCT_DEVICE := marlin
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 PRODUCT_MANUFACTURER := Google
-PRODUCT_RESTRICT_VENDOR_FILES := false
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NZH54D/4146044:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 7.1.2 NZH54D 4146044 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 9 PQ1A.190105.004 5148680 release-keys"
+    
+BUILD_FINGERPRINT="google/marlin/marlin:9/PQ1A.190105.004/5148680:user/release-keys"
+BUILD_THUMBPRINT="9/PQ1A.190105.004/5148680:user/release-keys"
 
 # Paranoid Android platform
 include vendor/pa/main.mk
