960cf9f0af8f0805d59c7fd87db8803b4b677252
Thecrazyskull <anaskarbila@gmail.com>
2019-02-12 13:25:03 -0500

    pa: sdm845
    ** Compilation instructions **
    
    * ./build/make/tools/roomservice.py sdm845
    * lunch (select sdm845-userdebug)
    * make systemimage
    
    Change-Id: I9778cf308d22e4d833632cbfb57d420c44a9ba23
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,6 +35,10 @@ add_lunch_combo pa_dora-userdebug
 add_lunch_combo pa_kagura-userdebug
 add_lunch_combo pa_keyaki-userdebug
 
+
+# CAF
+add_lunch_combo sdm845-userdebug
+
 # SDClang Environment Variables
 export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
 export SDCLANG_CONFIG=vendor/pa/sdclang/sdclang.json
