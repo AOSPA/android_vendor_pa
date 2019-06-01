725d2e6a885943d18e318991bb08fd60865a9887
TheStrix <parthbhatia98@gmail.com>
2019-06-01 12:57:07 +0530

    products: Add OnePlus 6/6T
    Change-Id: Iacdc399810f24a7557ae014cf81c227234fecad3

---
+++ products/oneplus6/pa_oneplus6.mk
@@ -0,0 +1,57 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_oneplus6,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_AOSP := false
+TARGET_USES_QCOM_BSP := false
+
+# Advanced platform features
+#TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+include device/qcom/common/common.mk
+include vendor/pa/main.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/oneplus/oneplus6/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_oneplus6
+PRODUCT_DEVICE := oneplus6
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := 6T
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR_PRODUCT_NAME := OnePlus6
+TARGET_VENDOR_DEVICE_NAME := OnePlus6
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus6 \
+    PRODUCT_NAME=OnePlus6 \
+    PRIVATE_BUILD_DESC="OnePlus6-user 8.1.0 OPM1.171019.011 273 release-keys"
+
+BUILD_FINGERPRINT := OnePlus/OnePlus6/OnePlus6:8.1.0/OPM1.171019.011/06140300:user/release-keys
+
+endif
