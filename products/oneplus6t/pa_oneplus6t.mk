aadbb3c3b90669646e302498dd4b5e2510507b0b
TheStrix <parthbhatia98@gmail.com>
2019-06-01 12:57:07 +0530

    products: Add OnePlus 6/6T
    Change-Id: Iacdc399810f24a7557ae014cf81c227234fecad3

---
+++ products/oneplus6t/pa_oneplus6t.mk
@@ -0,0 +1,53 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_oneplus6t,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_AOSP := false
+TARGET_USES_QCOM_BSP := false
+
+include device/qcom/common/common.mk
+include vendor/pa/main.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/oneplus/oneplus6t/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_oneplus6t
+PRODUCT_DEVICE := oneplus6t
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 6T
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR_PRODUCT_NAME := OnePlus6T
+TARGET_VENDOR_DEVICE_NAME := OnePlus6T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus6T \
+    PRODUCT_NAME=OnePlus6T \
+    PRIVATE_BUILD_DESC="OnePlus6T-user 9 PKQ1.180716.001 1812260627 release-keys"
+
+BUILD_FINGERPRINT := OnePlus/OnePlus6T/OnePlus6T:9/PKQ1.180716.001/1812260627:user/release-keys
+
+endif
