e2c0770ef69b904a5baa00c462a7132f1994712a
TheStrix <parthbhatia98@gmail.com>
2019-06-01 12:57:07 +0530

    products: Add OnePlus 6/6T
    Change-Id: Iacdc399810f24a7557ae014cf81c227234fecad3

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 Paranoid Android
+# Copyright (C) 2016-2019 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +19,8 @@
     $(LOCAL_DIR)/dipper/pa_dipper.mk \
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
+    $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
+    $(LOCAL_DIR)/oneplus6t/pa_oneplus6t.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
 
