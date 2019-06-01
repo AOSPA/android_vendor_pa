ab2edf9e106fb69e5d471d222f376020179061f2
TheStrix <parthbhatia98@gmail.com>
2019-06-01 12:57:07 +0530

    products: Add OnePlus 6/6T
    Change-Id: Iacdc399810f24a7557ae014cf81c227234fecad3

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,6 +21,8 @@
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
     $(LOCAL_DIR)/oneplus2/pa_oneplus2.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
+    $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
+    $(LOCAL_DIR)/oneplus6t/pa_oneplus6t.mk \
     $(LOCAL_DIR)/onyx/pa_onyx.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/robin/pa_robin.mk \
