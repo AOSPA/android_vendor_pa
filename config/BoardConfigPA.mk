28a35262a7cf66893f15d20465ccb07469dd5afa
LuK1337 <priv.luk@gmail.com>
2019-06-26 16:20:51 +0200

    pa: sepolicy: Add rules for FOD.
    Change-Id: I067ead0c2f60493a974bc220b67d7039acea4823

--- config/BoardConfigPA.mk
+++ config/BoardConfigPA.mk
@@ -1,14 +1,17 @@
 # Kernel
 include vendor/pa/config/BoardConfigKernel.mk
 
-# SEPolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    vendor/pa/sepolicy/private
-
 # QCOM flags
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 include vendor/pa/config/BoardConfigQcom.mk
 endif
+
+# SEPolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/pa/sepolicy/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/pa/sepolicy/public
 
 # Soong
 include vendor/pa/config/BoardConfigSoong.mk
