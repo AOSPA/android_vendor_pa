7459aee10f4cb7678f362a7893ce6b8f9d51d146
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2019-07-18 20:24:04 +0200

    products: Add OnePlus 7 Pro support
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Change-Id: I3f7e44b1bddf62d3b3c23645ac9054edcce559fe

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,6 +21,8 @@
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
     $(LOCAL_DIR)/oneplus6t/pa_oneplus6t.mk \
+    $(LOCAL_DIR)/oneplus7pro/pa_oneplus7pro.mk \
+    $(LOCAL_DIR)/onyx/pa_onyx.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
 
