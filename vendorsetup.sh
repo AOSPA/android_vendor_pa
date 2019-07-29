24cf6d285ebbdb6fb9318b42a3459e59b9fd775f
Jake Weinstein <jake@aospa.co>
2019-07-29 16:58:07 -0500

    vendor: remove deprecated devices
    Change-Id: I998da5328e5c980ba57a635fcf4efb34b5080bc4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,41 +1,16 @@
 # CAF
-add_lunch_combo pa_bacon-userdebug
 add_lunch_combo pa_beryllium-userdebug
 add_lunch_combo pa_dipper-userdebug
 add_lunch_combo pa_gemini-userdebug
-add_lunch_combo pa_oneplus2-userdebug
-add_lunch_combo pa_oneplus3-userdebug
-add_lunch_combo pa_onyx-userdebug
 add_lunch_combo pa_polaris-userdebug
-add_lunch_combo pa_r7plus-userdebug
-add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_sagit-userdebug
-add_lunch_combo pa_trltexx-userdebug
-add_lunch_combo pa_titan-userdebug
-add_lunch_combo pa_zl1-userdebug
 add_lunch_combo pa_oneplus6-userdebug
 add_lunch_combo pa_oneplus6t-userdebug
-
-# Samsung Exynos devices
-add_lunch_combo pa_gts210ltexx-userdebug
-add_lunch_combo pa_gts210wifi-userdebug
-
-# AOSP
-add_lunch_combo pa_angler-userdebug
-add_lunch_combo pa_bullhead-userdebug
-add_lunch_combo pa_hammerhead-userdebug
-add_lunch_combo pa_marlin-userdebug
-add_lunch_combo pa_sailfish-userdebug
-add_lunch_combo pa_shamu-userdebug
 
 # SONY OpenDevices
 # Loire
 add_lunch_combo pa_kugo-userdebug
 add_lunch_combo pa_suzu-userdebug
-# Tone
-add_lunch_combo pa_dora-userdebug
-add_lunch_combo pa_kagura-userdebug
-add_lunch_combo pa_keyaki-userdebug
 
 # SDClang Environment Variables
 export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
