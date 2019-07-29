e76941d8f6b2231bcd1204093f6d810a5c32fc22
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-07-29 22:09:47 +0200

    vendor: Add sanders
    Change-Id: I8115eb3777da28872b025cd45da27b80f0c781b4
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,6 +10,7 @@ add_lunch_combo pa_polaris-userdebug
 add_lunch_combo pa_r7plus-userdebug
 add_lunch_combo pa_robin-userdebug
 add_lunch_combo pa_sagit-userdebug
+add_lunch_combo pa_sanders-userdebug
 add_lunch_combo pa_trltexx-userdebug
 add_lunch_combo pa_titan-userdebug
 add_lunch_combo pa_zl1-userdebug
