20f041e423ac66e7ad0666b871820d01e23c3ba5
Jake Weinstein <jake@aospa.co>
2019-07-29 15:49:24 -0500

    vendor: remove duplicate libprotobuf-cpp-full
    This is already built by default
    
    Change-Id: Id2599868d57e05867a7676932521f01a285a6db7

--- main.mk
+++ main.mk
@@ -35,9 +35,6 @@ PRODUCT_COPY_FILES += vendor/pa/prebuilt/etc/privapp-permissions/pa-permissions.
 # Copy PA specific init file
 PRODUCT_COPY_FILES += vendor/pa/prebuilt/root/init.pa.rc:root/init.pa.rc
 
-# Include explicitly to work around GMS issues
-PRODUCT_PACKAGES += libprotobuf-cpp-full
-
 # Include support for additional filesystems
 PRODUCT_PACKAGES += \
     e2fsck \
