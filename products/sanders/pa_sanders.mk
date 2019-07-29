0155df2872a613818d3bb747d1a02b17c5ed8c4b
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-07-29 22:09:47 +0200

    [DNM] products: Add Moto G5S Plus (sanders)
    Change-Id: I8115eb3777da28872b025cd45da27b80f0c781b4
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

---
+++ products/sanders/pa_sanders.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_sanders,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/motorola/sanders/full_sanders.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sanders
+PRODUCT_NAME := pa_sanders
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_MODEL := Moto G (5S) Plus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto G5S Plus" \
+    PRIVATE_BUILD_DESC="sanders-user 8.1.0 OPS28.65-36 9fea release-keys"
+
+# Paranoid Android platform
+include device/qcom/common/common.mk
+include vendor/pa/main.mk
+
+endif
