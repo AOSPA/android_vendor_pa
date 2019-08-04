737b6d3ac156b00ffa1f2140fd3adbd6d67cb0fd
Akash <akashniki@gmail.com>
2019-08-04 19:19:45 +0000

    products: Add Xiaomi MI 9
    Change-Id: I29ab0b881c6cc4f52569b8a5eb2cf6c38a251b73

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,6 +16,7 @@
 # CAF
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/beryllium/pa_beryllium.mk \
+    $(LOCAL_DIR)/cepheus/pa_cepheus.mk \
     $(LOCAL_DIR)/davinci/pa_davinci.mk \
     $(LOCAL_DIR)/dipper/pa_dipper.mk \
     $(LOCAL_DIR)/gemini/pa_gemini.mk \
@@ -40,6 +41,7 @@ PRODUCT_MAKEFILES += \
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     pa_beryllium-userdebug \
+    pa_cepheus-userdebug \
     pa_davinci-userdebug \
     pa_dipper-userdebug \
     pa_dora-userdebug \
