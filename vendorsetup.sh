2cfeb8161eaa18be44e4841e660af9ac05c89bb9
Marco Marinho <marcomarinho12@gmail.com>
2019-08-08 11:57:21 +0100

    products: Add OnePlus5(t)
    * big thanks to @xboxfanj for all the help
    
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>
    Change-Id: I6a596e72ace6e24d64103845f9f536181efabd9f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,7 @@ add_lunch_combo pa_beryllium-userdebug
 add_lunch_combo pa_dipper-userdebug
 add_lunch_combo pa_gemini-userdebug
 add_lunch_combo pa_oneplus3-userdebug
+add_lunch_combo pa_oneplus5-userdebug
 add_lunch_combo pa_oneplus6-userdebug
 add_lunch_combo pa_oneplus6t-userdebug
 add_lunch_combo pa_polaris-userdebug
