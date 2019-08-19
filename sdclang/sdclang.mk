357fbcbfab0a561b04b1c8d4d67550d63903a390
Jake Weinstein <jake@aospa.co>
2019-08-19 04:49:28 +0200

    vendor: update SDCLANG configuration to 6.0 for Pie
    6.0.9 passes CTS and has no stability issues.
    
    Change-Id: I33ba5c12f083c8b75cd7ba9c2c67ae4ab3101e4d

--- sdclang/sdclang.mk
+++ sdclang/sdclang.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 ParanoidAndroid Project
+# Copyright (C) 2016-2019 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
