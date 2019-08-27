58267a192c635eee14f9dbc932595652aa743e8d
spezi77 <spezi7713@gmx.net>
2019-08-27 21:58:29 +0200

    vendor/pa: soong: Add Uses_qcom_um_family flag.
    If a device needs CAF extensions in frameworks/native set the flag
    TARGET_USES_QCOM_UM_FAMILY := true
    
    Change-Id: If16013359c85a941e02737d012aa1804f08b4af5
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,14 @@ type Product_variables struct {
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
+	Uses_qcom_um_family struct {
+		Cflags []string
+		Srcs []string
+	}
 }
 
 type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
+	Uses_qcom_um_family  *bool `json:",omitempty"`
 }
