698e0010ccbb779470bc58d4d57845ae193908e7
TheStrix <parthbhatia98@gmail.com>
2019-09-05 21:32:31 +0530

    pa: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Change-Id: I088b93e324cd39377e2bcc1bf5203f639e494115

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,3 @@
-# CAF
-add_lunch_combo pa_beryllium-userdebug
-add_lunch_combo pa_dipper-userdebug
-add_lunch_combo pa_gemini-userdebug
-add_lunch_combo pa_oneplus3-userdebug
-add_lunch_combo pa_oneplus6-userdebug
-add_lunch_combo pa_oneplus6t-userdebug
-add_lunch_combo pa_polaris-userdebug
-add_lunch_combo pa_sagit-userdebug
-
-# SONY OpenDevices
-# Loire
-add_lunch_combo pa_kugo-userdebug
-add_lunch_combo pa_suzu-userdebug
-# Tone
-add_lunch_combo pa_dora-userdebug
-add_lunch_combo pa_kagura-userdebug
-add_lunch_combo pa_keyaki-userdebug
-
 # SDClang Environment Variables
 export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
 export SDCLANG_CONFIG=vendor/pa/sdclang/sdclang.json
