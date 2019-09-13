f548039fb62a2856fe861559ae3760bf0a1a6d44
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-13 16:22:36 -0300

    vendor: Add support for dark bootanimation
    The dark theme state is stored on misc partition, that follow pixel behavior
    
    As reference:
    
    https://android.googlesource.com/device/google/crosshatch/+/refs/tags/android-10.0.0_r2/init.hardware.rc#994
    
    https://android.googlesource.com/platform/bootable/recovery/+/35e0f6d290ea7c12164bcfaf1b857d965e943938
    
    Change-Id: Ia2d9d163f84638d0c61b3d2e7161301aad7fc774

--- config/common.mk
+++ config/common.mk
@@ -168,6 +168,14 @@ PRODUCT_PACKAGES += \
 # Move Wi-Fi modules to vendor
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
+# Themed bootanimation
+TARGET_MISC_BLOCK_OFFSET ?= 0
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.misc.block.offset=$(TARGET_MISC_BLOCK_OFFSET)
+PRODUCT_PACKAGES += \
+    misc_writer_system \
+    themed_bootanimation
+
 # SECCOMP Extension
 BOARD_SECCOMP_POLICY += vendor/pa/seccomp
 
