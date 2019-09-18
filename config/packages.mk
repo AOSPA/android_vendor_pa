ea73f8812855c16114e648427f49dad21783f26c
TheStrix <parthbhatia98@gmail.com>
2019-09-18 23:24:13 +0530

    pa: Restructure config files
     - Set version props in build/core/
     - Add new config targets for devices to inherit from, common_full_phone & common_full_tablet_wifionly.
     - Update the config inherit path for all pa devices.
     - Remove deprecated hostapd configs.
     - todo: Remove pa privapp configs for now. Need to update/adapt to new style.
     - Add init.d support.
     - Add init scripts (prebuilt/etc/init) from LineageOS.
     - Update few permission configs from LineageOS.
    
    Change-Id: I86791110eb698d7ba43cc05051345dc08b215ce3

---
+++ config/packages.mk
@@ -0,0 +1,87 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Paranoid Android packages
+ifneq ($(TARGET_USES_AOSP_CAMERA),true)
+PRODUCT_PACKAGES += ParanoidCamera
+endif
+PRODUCT_PACKAGES += \
+    ChromePublic \
+    ParanoidPapers \
+    SoundRecorder \
+    WallpaperPicker \
+    LatinIME \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    #ParanoidHub
+
+# Snapdragon apps
+PRODUCT_PACKAGES += \
+    SnapdragonGallery \
+    SnapdragonMusic
+
+# CAF packages
+# TCP Connection Management
+PRODUCT_PACKAGES += tcmiface
+PRODUCT_BOOT_JARS += tcmiface
+
+# Bluetooth Audio (A2DP)
+PRODUCT_PACKAGES += libbthost_if
+
+# MSIM manual provisioning
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
+# Extra tools in PA
+PRODUCT_PACKAGES += \
+    7z \
+    awk \
+    bash \
+    bzip2 \
+    curl \
+    getcap \
+    htop \
+    lib7z \
+    libsepol \
+    pigz \
+    powertop \
+    setcap \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
+
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.exfat \
+    mkfs.ntfs \
+    mount.ntfs
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
