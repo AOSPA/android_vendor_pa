ab81d8ec2d42b3b1d2cd5eae7bb50dddc7275a87
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 20:18:11 +0200

    prebuilt: backuptool: Implement dynamic system mount for A-only
    * Dynamically mount system to the path chosen by the recovery through backuptool
    * This can be helpful because of the fragmentation that will happen with system mount in recovery after Q
    
    Change-Id: I2d1e775efcf87e33319bc7790d1e54bca72116d3

--- prebuilt/bin/backuptool.sh
+++ prebuilt/bin/backuptool.sh
@@ -1,17 +1,19 @@
 #!/sbin/sh
 #
-# Backup and restore addon $S files
+# Backup and restore addon /system files
 #
 
 export C=/tmp/backupdir
-export S=$2
+export SYSDEV="$(readlink -nf "$2")"
+export SYSFS="$3"
+export V=17.1
 
 export ADDOND_VERSION=1
 
-# Scripts in $S/addon.d expect to find backuptool.functions in /tmp
+# Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
 
-# Preserve $S/addon.d in /tmp/addon.d
+# Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
   if [ -d $S/addon.d/ ]; then
     mkdir -p /tmp/addon.d/
@@ -32,7 +34,7 @@ preserve_addon_d() {
   fi
 }
 
-# Restore $S/addon.d from /tmp/addon.d
+# Restore /system/addon.d from /tmp/addon.d
 restore_addon_d() {
   if [ -d /tmp/addon.d/ ]; then
     mkdir -p $S/addon.d/
@@ -41,8 +43,50 @@ restore_addon_d() {
   fi
 }
 
+# Proceed only if /system is the expected major and minor version
+check_prereq() {
+# If there is no build.prop file the partition is probably empty.
+if [ ! -r $S/build.prop ]; then
+    return 0
+fi
+if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
+  echo "Not backing up files from incompatible version: $V"
+  return 0
+fi
+return 1
+}
 
-# Execute $S/addon.d/*.sh scripts with $1 parameter
+check_blacklist() {
+  if [ -f $S/addon.d/blacklist -a -d /$1/addon.d/ ]; then
+      ## Discard any known bad backup scripts
+      cd /$1/addon.d/
+      for f in *sh; do
+          [ -f $f ] || continue
+          s=$(md5sum $f | cut -c-32)
+          grep -q $s $S/addon.d/blacklist && rm -f $f
+      done
+  fi
+}
+
+check_whitelist() {
+  found=0
+  if [ -f $S/addon.d/whitelist ];then
+      ## forcefully keep any version-independent stuff
+      cd /$1/addon.d/
+      for f in *sh; do
+          s=$(md5sum $f | cut -c-32)
+          grep -q $s $S/addon.d/whitelist
+          if [ $? -eq 0 ]; then
+              found=1
+          else
+              rm -f $f
+          fi
+      done
+  fi
+  return $found
+}
+
+# Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
 if [ -d /tmp/addon.d/ ]; then
   for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
@@ -51,21 +95,65 @@ if [ -d /tmp/addon.d/ ]; then
 fi
 }
 
+determine_system_mount() {
+  if grep -q -e"^$SYSDEV" /proc/mounts; then
+    umount $(grep -e"^$SYSDEV" /proc/mounts | cut -d" " -f2)
+  fi
+
+  if [ -d /mnt/system ]; then
+    SYSMOUNT="/mnt/system"
+  elif [ -d /system_root ]; then
+    SYSMOUNT="/system_root"
+  else
+    SYSMOUNT="/system"
+  fi
+
+  export S=$SYSMOUNT/system
+}
+
+mount_system() {
+  mount -t $SYSFS $SYSDEV $SYSMOUNT -o rw,discard
+}
+
+unmount_system() {
+  umount $SYSMOUNT
+}
+
+determine_system_mount
+
 case "$1" in
   backup)
+    mount_system
     mkdir -p $C
+    if check_prereq; then
+        if check_whitelist system; then
+            unmount_system
+            exit 127
+        fi
+    fi
+    check_blacklist system
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
+    unmount_system
   ;;
   restore)
+    mount_system
+    if check_prereq; then
+        if check_whitelist tmp; then
+            unmount_system
+            exit 127
+        fi
+    fi
+    check_blacklist tmp
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
     restore_addon_d
     rm -rf $C
     sync
+    unmount_system
   ;;
   *)
     echo "Usage: $0 {backup|restore}"
