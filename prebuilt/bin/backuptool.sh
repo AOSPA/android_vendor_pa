27f1b89285b11ae40c54f50b6ff66f7ac52593fd
Davide Garberi <dade.garberi@gmail.com>
2019-09-24 20:18:11 +0200

    prebuilt: backuptool: Implement dynamic system mount for A-only
    * Dynamically mount system to the path chosen by the recovery through backuptool
    * This can be helpful because of the fragmentation that will happen with system mount in recovery after Q
    
    Change-Id: I2d1e775efcf87e33319bc7790d1e54bca72116d3

--- prebuilt/bin/backuptool.sh
+++ prebuilt/bin/backuptool.sh
@@ -1,17 +1,17 @@
 #!/sbin/sh
 #
-# Backup and restore addon $S files
+# Backup and restore addon /system files
 #
 
 export C=/tmp/backupdir
-export S=$2
+export sysdev=$2
 
 export ADDOND_VERSION=1
 
-# Scripts in $S/addon.d expect to find backuptool.functions in /tmp
+# Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
 
-# Preserve $S/addon.d in /tmp/addon.d
+# Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
   if [ -d $S/addon.d/ ]; then
     mkdir -p /tmp/addon.d/
@@ -32,7 +32,7 @@ preserve_addon_d() {
   fi
 }
 
-# Restore $S/addon.d from /tmp/addon.d
+# Restore /system/addon.d from /tmp/addon.d
 restore_addon_d() {
   if [ -d /tmp/addon.d/ ]; then
     mkdir -p $S/addon.d/
@@ -41,8 +41,7 @@ restore_addon_d() {
   fi
 }
 
-
-# Execute $S/addon.d/*.sh scripts with $1 parameter
+# Execute /system/addon.d/*.sh scripts with $1 parameter
 run_stage() {
 if [ -d /tmp/addon.d/ ]; then
   for script in $(find /tmp/addon.d/ -name '*.sh' |sort -n); do
@@ -51,21 +50,51 @@ if [ -d /tmp/addon.d/ ]; then
 fi
 }
 
+system() {
+  if [[ ! -z $(mount | grep system) ]]; then
+    umount $(mount | grep system | sed "s|.*on ||;s| .*||")
+  fi
+
+  if [ -d /mnt/system ]; then
+    sysmount="/mnt/system"
+  elif [ -d /system_root ]; then
+    sysmount="/system_root"
+  else
+    sysmount="/system"
+  fi
+
+  export S=$sysmount/system
+}
+
+mountsys() {
+  mount -t ext4 $sysdev $sysmount -o rw,discard
+}
+
+umountsys() {
+  umount $sysmount
+}
+
+system
+
 case "$1" in
   backup)
+    mountsys
     mkdir -p $C
     preserve_addon_d
     run_stage pre-backup
     run_stage backup
     run_stage post-backup
+    umountsys
   ;;
   restore)
+    mountsys
     run_stage pre-restore
     run_stage restore
     run_stage post-restore
     restore_addon_d
     rm -rf $C
     sync
+    umountsys
   ;;
   *)
     echo "Usage: $0 {backup|restore}"
