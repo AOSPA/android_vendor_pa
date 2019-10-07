a405d03b61505b54bbfcd07d315ca6cd07adfa1f
MrMesquita <mickaelmendes50@gmail.com>
2019-10-07 19:54:00 +0000

    [DNM] products: beryllium dev trees for Q
    Change-Id: I6734175d7eb1e50bf63718ee229973eb91049445
    Signed-off-by: MrMesquita <mickaelmendes50@gmail.com>

--- products/beryllium/pa_beryllium.mk
+++ products/beryllium/pa_beryllium.mk
@@ -15,10 +15,6 @@
 # Check for target product
 ifeq (pa_beryllium,$(TARGET_PRODUCT))
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 # Include Paranoid Android common configuration
 TARGET_BOOT_ANIMATION_RES := 1080
 
@@ -38,13 +34,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:8.1.0/OPM1.171019.011/V9.6.18.0.OEJMIFD:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 8.1.0 OPM1.171019.011 V9.6.18.0.OEJMIFD release-keys" \
-    PRODUCT_NAME="beryllium" \
-    TARGET_DEVICE="beryllium"
+    PRODUCT_DEVICE="beryllium" \
+    PRODUCT_NAME="beryllium"
 
 endif
