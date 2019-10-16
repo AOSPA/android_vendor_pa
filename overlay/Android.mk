81d52d7997cf591f3f78458440d44d8554ccd4a0
Chris Crump <chris@cypheros.co>
2019-10-16 11:42:05 -0400

    pa: Remove deprecated overlays.
    Change-Id: Id7fd1cb89d7b568b56831211880831b484932045

--- overlay/Android.mk
+++ overlay/Android.mk
@@ -25,7 +25,6 @@ LOCAL_REQUIRED_MODULES := \
     AOSPABluetoothOverlay \
     AOSPAContactsProviderOverlay \
     AOSPAFrameworksOverlay \
-    AOSPAKeyguardOverlay \
     AOSPAOverlayStub \
     AOSPASettingsOverlay \
     AOSPASettingsProviderOverlay \
