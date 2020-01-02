7eac0c6502757436b6992c42f9763be5e8b8d9f2
YumeMichi <do4suki@gmail.com>
2020-01-02 19:30:41 +0800

    [WIP] dipper: Update to Quartz
    Change-Id: Ic852c22eeb150312b96d967a7f9381423317db7b
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- products/dipper/pa_dipper.mk
+++ products/dipper/pa_dipper.mk
@@ -35,13 +35,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.11.0.OEACNFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEACNFA release-keys" \
-    PRODUCT_NAME="dipper" \
-    TARGET_DEVICE="dipper"
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
+    PRODUCT_DEVICE="dipper" \
+    PRODUCT_NAME="dipper"
 
 endif
