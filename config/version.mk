8a3eae6c2fff57d9565b6a8bcfece9d62f106a3a
TheStrix <parthbhatia98@gmail.com>
2020-01-06 21:43:56 +0530

    pa: version: Fix version string for release type
     - Avoid an extra '-' at the end for PA_BUILDTYPE=RELEASE.
    
    Change-Id: Ia14f3daa9abc09426e88f812a272eaa7045ea24a

--- config/version.mk
+++ config/version.mk
@@ -64,6 +64,8 @@ endif
 
 ifneq ($(filter Alpha Beta,$(PA_BUILD_VARIANT)),)
   PA_VERSION := $(shell echo $(PA_VERSION_FLAVOR) | tr A-Z a-z)-$(shell echo $(PA_BUILD_VARIANT) | tr A-Z a-z)-$(PA_VERSION_CODE)-$(PA_BUILD)-$(BUILD_DATE)
+else ifeq ($(PA_BUILD_VARIANT),)
+  PA_VERSION := $(shell echo $(PA_VERSION_FLAVOR) | tr A-Z a-z)-$(PA_VERSION_CODE)-$(PA_BUILD)-$(BUILD_DATE)
 else
   PA_VERSION := $(shell echo $(PA_VERSION_FLAVOR) | tr A-Z a-z)-$(PA_VERSION_CODE)-$(PA_BUILD)-$(BUILD_DATE)-$(shell echo $(PA_BUILD_VARIANT) | tr A-Z a-z)
 endif
