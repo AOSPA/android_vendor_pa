9cf2c53b5072e816ed3ebd18b2e422e7aeeab32a
ZVNexus <zvnexus@outlook.com>
2020-01-10 07:47:49 -0500

    pa: Update OnePlus 3/T for Android 10.
    Change-Id: I96b3c5f2f9f4692b09609130eeb2d37a77da6d7c

--- products/oneplus3/pa_oneplus3.mk
+++ products/oneplus3/pa_oneplus3.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 Paranoid Android
+# Copyright (C) 2016-2020 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,31 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Check for target product
+# Ensure OnePlus 3/T is the target product.
 ifeq (pa_oneplus3,$(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Inherit device configuration
-$(call inherit-product, device/oneplus/oneplus3/oneplus3.mk)
+# Inherit our custom device configuration.
+$(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit common PA configuration
 $(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
-# Override build properties
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Override the AOSP build properties.
 PRODUCT_NAME := pa_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-     BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/10122109:user/release-keys \
-     PRIVATE_BUILD_DESC="OnePlus3-user 7.1.1 NMF26F 114 dev-keys"
 
 endif
