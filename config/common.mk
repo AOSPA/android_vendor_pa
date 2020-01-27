07732af7dcbb467b8fe4fcff1fdd414825a40858
Jake Weinstein <jake@aospa.co>
2020-01-27 02:41:18 +0100

    pa: Use QCOM Legacy SEPolicies by default for Legacy Boards
    You can also manually choose these in your BoardConfig
    
    Change-Id: Iab13d3f6d2e33964899c77ff0fa8e11467a5fa73

--- config/common.mk
+++ config/common.mk
@@ -140,5 +140,16 @@ ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
   ALLOW_MISSING_DEPENDENCIES := true
 endif
 
+# Use QCOM Legacy UM SEPolicy by default for legacy UM boards
+ifeq ($(call is-board-platform-in-list, msm8909 msm8937 msm8953 msm8996 msm8998 sdm660),true)
+  TARGET_USES_QCOM_LEGACY_UM_SEPOLICY ?= true
+endif
+
+# Use QCOM Legacy SEPolicy by default for legacy pre-UM boards
+ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8952 msm8960 msm8974 msm8976 msm8992 msm8994),true)
+  TARGET_USES_QCOM_LEGACY_SEPOLICY ?= true
+endif
+
+
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/partner_gms/products/turbo.mk)
