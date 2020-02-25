f857c961921a89fdf23d508db9af424b412f5998
Stylogey <stylogey@gmail.com>
2020-02-25 16:55:41 +0100

    pa: Add Xiaomi Mi A2
    Change-Id: Ife38e6151205ed8b9cdd1252aeffb7bc1489438b

---
+++ products/jasmine_sprout/pa_jasmine_sprout.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_jasmine_sprout,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+include device/qcom/common/common.mk
+include vendor/pa/config/common_full_phone.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_jasmine_sprout
+PRODUCT_DEVICE := jasmine_sprout
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A2
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
+
+endif
