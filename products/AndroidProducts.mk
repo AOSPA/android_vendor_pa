61caa8f98f3b9f266096669f4bae70e81785a4da
mahajant99 <mahajant99@gmail.com>
2020-02-27 11:54:01 +0530

    pa: Add Sakura
    Signed-off-by: mahajant99 <mahajant99@gmail.com>
    Change-Id: I74b2bc1f902b7c00e06cb4f203d5085a84c56f6d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -34,6 +34,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/rolex/pa_rolex.mk \
     $(LOCAL_DIR)/rosy/pa_rosy.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
+    $(LOCAL_DIR)/sakura/pa_sakura.mk \
     $(LOCAL_DIR)/whyred/pa_whyred.mk \
     $(LOCAL_DIR)/X00TD/pa_X00TD.mk \
     $(LOCAL_DIR)/X01BD/pa_X01BD.mk
@@ -74,6 +75,7 @@ COMMON_LUNCH_CHOICES += \
     pa_rolex-userdebug \
     pa_rosy-userdebug \
     pa_sagit-userdebug \
+    pa_sakura-userdebug \
     pa_suzu-userdebug \
     pa_whyred-userdebug \
     pa_X00TD-userdebug \
