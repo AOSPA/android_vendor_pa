63f097e254a6a2b09d458f05277ab5407cd4648e
Arian <arian.kulmer@web.de>
2020-03-09 14:09:24 +0100

    Revert "pa: Ship Device Personalization Services"
    This reverts commit 18770454fa6d24cc15229ffb381bb20b26dead2b.
    
    Change-Id: I08b91e451f3215275208c145afd3d40a962c9d80

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -32,18 +32,6 @@ LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := MatchmakerPrebuilt
-LOCAL_SRC_FILES     := priv-app/MatchmakerPrebuilt.apk
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := APPS
-LOCAL_CERTIFICATE   := PRESIGNED
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_DEX_PREOPT    := false
-LOCAL_PRODUCT_MODULE := true
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE        := Longshot
 LOCAL_SRC_FILES     := app/Longshot.apk
 LOCAL_MODULE_TAGS   := optional
