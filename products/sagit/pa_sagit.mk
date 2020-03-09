50fec2a9b2c4dad1f6702a8cb8f0dc9136474cac
Arian <arian.kulmer@web.de>
2020-03-09 14:16:09 +0100

    pa: Update sagit to quartz
    Change-Id: If33cd5da2457dfae4541789dbecba1c56cd8b49a

--- products/sagit/pa_sagit.mk
+++ products/sagit/pa_sagit.mk
@@ -35,9 +35,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.5.8.0.OCAMIFA:user/release-keys \
-    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.5.8.0.OCAMIFA release-keys"
+    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
+
 endif
