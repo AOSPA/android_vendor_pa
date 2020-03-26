128b2b0015170a6d4494571ee0c3857100420353
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2020-03-26 18:36:24 +0000

    pa: adapt for gapps.
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Change-Id: I56014684c3d621d50925d5487f3f89c103c24011

--- config/common.mk
+++ config/common.mk
@@ -84,11 +84,6 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/pa/prebuilt/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
 	vendor/pa/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
-# Markup Google
-PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
-    vendor/pa/prebuilt/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
-
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
@@ -139,11 +134,6 @@ endif
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
-# Use default filter for problematic apps
-PRODUCT_DEXPREOPT_QUICKEN_APPS += \
-    Dialer \
-    ChromePublic
-
 # Enable ALLOW_MISSING_DEPENDENCIES on Vendorless Builds
 ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
   ALLOW_MISSING_DEPENDENCIES := true
@@ -181,5 +171,28 @@ PRODUCT_COPY_FILES += \
     vendor/pa/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     vendor/pa/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+ifneq ($(TARGET_DISABLES_GAPPS), true)
+
+# set arch for gapps
+ifeq ($(TARGET_ARCH),arm64)
+TARGET_GAPPS_ARCH := arm64
+else ifeq ($(TARGET_ARCH),arm)
+TARGET_GAPPS_ARCH := arm
+endif
+
+# Inherit GApps Makefiles
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/partner_gms/products/turbo.mk)
+$(call inherit-product-if-exists, vendor/gapps/config.mk)
+
+# Do not preoptimize prebuilts when building GApps
+DONT_DEXPREOPT_PREBUILTS := true
+
+else
+
+# Use default filter for problematic AOSP apps
+PRODUCT_DEXPREOPT_QUICKEN_APPS += \
+    Dialer \
+    ChromePublic
+
+endif #TARGET_DISABLES_GAPPS
