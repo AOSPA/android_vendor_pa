d0fd145dbc3aef29b0788bc4ff6e2b488aee780e
Jake Weinstein <jake@aospa.co>
2020-03-29 17:35:08 +0200

    pa: config: qcom: Enable TARGET_USES_MEDIA_EXTENSIONS on legacy SoCs
    This needs to be in BoardConfig so the SoC can be detected.  We will
    also move all board flags to this file in subsequent commits.
    
    Change-Id: I5e60c482a18b480fca9a77fabc9f815413e494d3

--- config/BoardConfigPA.mk
+++ config/BoardConfigPA.mk
@@ -1,5 +1,10 @@
 # Kernel
 include vendor/pa/config/BoardConfigKernel.mk
 
+# QCOM flags
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+include vendor/pa/config/BoardConfigQcom.mk
+endif
+
 # Soong
 include vendor/pa/config/BoardConfigSoong.mk
