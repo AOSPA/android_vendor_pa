f075526ee7ec8423f8ada919882392c39293aee1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-03-29 11:19:25 +0530

    config: Add BoardConfigQcom.mk for platform-specific config
    Change-Id: I8321d221491cc3869e6e41da3a2bd17debdbab8a

--- config/BoardConfigPA.mk
+++ config/BoardConfigPA.mk
@@ -3,3 +3,8 @@ include vendor/pa/config/BoardConfigKernel.mk
 
 # Soong
 include vendor/pa/config/BoardConfigSoong.mk
+
+# QCOM flags
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+include device/qcom/common/BoardConfigQcom.mk
+endif
