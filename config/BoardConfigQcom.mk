a0c996258cc1b9b84878a9f58684f1520db36e6d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-08 02:00:18 +0530

    config: Enable TARGET_USES_QCOM_BSP for pre-UM devices
    Change-Id: I95e3371d7f6443259972295d5569628c917d3f0c

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -26,3 +26,8 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 msm8952 msm8953 msm8960 msm8974 msm8976 msm8992 msm8994 msm8996 msm8998 sdm660),true)
   TARGET_USES_MEDIA_EXTENSIONS := true
 endif
+
+# For pre-UM display and gps HAL
+ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8610 msm8974 msm8992 msm8994 msm8909 msm8916 msm8952 msm8976),true)
+  TARGET_USES_QCOM_BSP := true
+endif
