ad235f1159cd34aad01220fb63b9289696499ea8
Alexander Koskovich <zvnexus@outlook.com>
2020-04-08 13:15:44 -0400

    pa: Add Google Pixel 3a/XL.
    Change-Id: I3a10ce717800700a19186ac8097c03b985f578c4

---
+++ products/sargo/pa_sargo.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Ensure OnePlus 3/T is the target product.
+ifeq (pa_sargo,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit our custom device configuration.
+$(call inherit-product, device/google/bonito/device-sargo.mk)
+
+# Inherit common PA configuration
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Override the AOSP build properties.
+PRODUCT_NAME := pa_sargo
+PRODUCT_DEVICE := sargo
+PRODUCT_BRAND := Google
+PRODUCT_MANUFACTURER := Google
+PRODUCT_MODEL := Google Pixel 3a
+
+endif
