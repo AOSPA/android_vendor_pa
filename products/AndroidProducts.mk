f2f80d958f1f21c9a9b2f7093a095ae32f2be218
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-20 02:24:39 +0530

    pa: Add Xiaomi Redmi Note 8/8T (ginkgo)
    Change-Id: I6c72d080b06b31d715490f30b0b5d6205a4ef4fa

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,6 +21,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/chiron/pa_chiron.mk \
     $(LOCAL_DIR)/davinci/pa_davinci.mk \
     $(LOCAL_DIR)/dipper/pa_dipper.mk \
+    $(LOCAL_DIR)/ginkgo/pa_ginkgo.mk \
     $(LOCAL_DIR)/land/pa_land.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
@@ -58,6 +59,7 @@ COMMON_LUNCH_CHOICES += \
     pa_davinci-userdebug \
     pa_dipper-userdebug \
     pa_dora-userdebug \
+    pa_ginkgo-userdebug \
     pa_kagura-userdebug \
     pa_keyaki-userdebug \
     pa_kugo-userdebug \
