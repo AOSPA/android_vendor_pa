502890d80cd0447f27a19a02d81ad0b18e60af8a
David Sehr <sehr@google.com>
2020-05-01 13:18:57 -0700

    [android-11.0.0_r15] pa: enable dex2oat64 on 64-bit targets
    Bug: 153380900
    Test: boot and install application.
    Change-Id: Iac14c028c1c5c7aa4a9173d51d63ef5834ba690a
    Merged-In: Iac14c028c1c5c7aa4a9173d51d63ef5834ba690a

--- config/common.mk
+++ config/common.mk
@@ -19,6 +19,11 @@ PRODUCT_COPY_FILES += \
 # ART
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS), true)
+# Use 64-bit dex2oat for better dexopt time.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true
+endif
 
 # Boot Animation
 ifneq ($(TARGET_BOOT_ANIMATION_RES),)
