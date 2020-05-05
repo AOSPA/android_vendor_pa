391485e12046a8d877c91cddd6b199503f603e00
Tim <tomgus1.2@gmail.com>
2020-05-05 21:24:19 +0200

    pa: It's tama time
    Added configs for Sony Tama devices
    
    Change-Id: I33460adf4906d2c9d233c4ddb6722b557c799078

---
+++ products/akatsuki/pa_akatsuki.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_akatsuki,$(TARGET_PRODUCT))
+
+# Use the AOSP stack
+TARGET_USES_AOSP := true
+TARGET_EXCLUDE_QCOM_SEPOLICY := true
+BOARD_USE_ENFORCING_SELINUX := false
+BUILD_WITHOUT_VENDOR := true
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/sony/akatsuki/aosp_h8416.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_akatsuki
+PRODUCT_MODEL := Xperia XZ3 (AOSPA)
+
+BUILD_FINGERPRINT := Sony/H8416/H8416:9/52.0.A.3.27/2935389866:user/release-keys
+
+# Paranoid Android platform
+include vendor/pa/config/common_full_phone.mk
+
+endif
---
+++ products/akatsuki_dual/pa_akatsuki_dual.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_akatsuki_dual,$(TARGET_PRODUCT))
+
+# Use the AOSP stack
+TARGET_USES_AOSP := true
+TARGET_EXCLUDE_QCOM_SEPOLICY := true
+BOARD_USE_ENFORCING_SELINUX := false
+BUILD_WITHOUT_VENDOR := true
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/sony/akatsuki/aosp_h9436.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_akatsuki_dual
+PRODUCT_MODEL := Xperia XZ3 Dual (AOSPA)
+
+BUILD_FINGERPRINT := Sony/H9436/H9436:9/52.0.A.3.27/2935389866:user/release-keys
+
+# Paranoid Android platform
+include vendor/pa/config/common_full_phone.mk
+
+endif
