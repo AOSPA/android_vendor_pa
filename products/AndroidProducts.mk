21443ddd3fddf2b68208694dc08cc70ab675aa6a
sweetyicecare <gabrielseedev@outlook.com>
2020-05-07 21:24:11 +0700

    products: Add Xiaomi Redmi 3s (land)
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>
    Change-Id: Id9aefe7cfe94a88491f255dd69a28731bd4f55c7

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -21,6 +21,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/chiron/pa_chiron.mk \
     $(LOCAL_DIR)/davinci/pa_davinci.mk \
     $(LOCAL_DIR)/dipper/pa_dipper.mk \
+    $(LOCAL_DIR)/land/pa_land.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
@@ -61,6 +62,7 @@ COMMON_LUNCH_CHOICES += \
     pa_keyaki-userdebug \
     pa_kugo-userdebug \
     pa_mata-userdebug \
+    pa_land-userdebug \
     pa_oneplus3-userdebug \
     pa_oneplus6-userdebug \
     pa_oneplus7-userdebug \
