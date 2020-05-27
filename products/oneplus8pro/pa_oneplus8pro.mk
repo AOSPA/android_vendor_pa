43b94a9e6635a2045ae22f86ec655f8faaef1d1f
Chris Crump <chris@cypheros.co>
2020-05-27 17:22:09 -0400

    pa: products: Add Oneplus 8 Pro (instantnoodle)
    Change-Id: I63ce4e9dfedfc9472cd10ca947529a525a0abe18

---
+++ products/oneplus8pro/pa_oneplus8pro.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_oneplus8pro,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Inherit device configuration
+$(call inherit-product, device/oneplus/oneplus8/device.mk)
+
+# Inherit common PA configuration
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+# Override build properties
+PRODUCT_NAME := pa_oneplus8pro
+PRODUCT_DEVICE := oneplus8pro
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := IN2023
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=OnePlus8Pro \
+    PRODUCT_NAME=OnePlus8Pro \
+    PRIVATE_BUILD_DESC="OnePlus8Pro-user-10-QKQ1.191222.002-2005100226-release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+
+endif
