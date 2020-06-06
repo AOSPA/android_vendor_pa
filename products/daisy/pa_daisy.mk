5365efec622c7960bdd2294eb23b6d500b089d1f
vitor00almei <pondebatata@outlook.com>
2020-06-06 00:48:10 +0000

    PA: products: add Mi A2 Lite (Daisy)
    Change-Id: I65c5b9176cc69f4b565cdfd9d7fd16ed713ed0f4

---
+++ products/daisy/pa_daisy.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_daisy,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from daisy device
+$(call inherit-product, device/xiaomi/daisy/device.mk)
+
+# Include Paranoid Android common configuration
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := daisy
+PRODUCT_NAME := pa_daisy
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A2 Lite
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="daisy-user 9 PKQ1.180917.001 V10.0.18.0.PDLMIXM release-keys" \
+    PRODUCT_NAME="daisy_sprout" \
+    TARGET_DEVICE="daisy"
+
+endif
