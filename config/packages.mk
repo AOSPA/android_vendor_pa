9c22a4bea68bbe262f68b9ee8c927f6f48783f8f
Jake Weinstein <jake@aospa.co>
2020-06-10 06:15:57 +0200

    pa: Recategorize packages
    - Alphabetize
    - Move telephony items to telephony
    - Move packages from common.mk to packages.mk
    
    Change-Id: I8d5e9cb86416de240942669eaf34ebbf0946512f

--- config/packages.mk
+++ config/packages.mk
@@ -12,20 +12,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Paranoid Android packages
-ifneq ($(TARGET_USES_AOSP_CAMERA),true)
-PRODUCT_PACKAGES += ParanoidCamera
-endif
+# Abstruct
+PRODUCT_PACKAGES += Abstruct
+
+# AOSP Packages
 PRODUCT_PACKAGES += \
     SoundRecorder \
     WallpaperPicker \
     LatinIME \
     LiveWallpapers \
     LiveWallpapersPicker \
+    ThemePicker
+
+# Bluetooth Audio (A2DP)
+PRODUCT_PACKAGES += libbthost_if
+
+# Charger mode images
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+ifneq ($(TARGET_USES_AOSP_CHARGER),true)
+PRODUCT_PACKAGES += \
+    product_charger_res_images
+endif
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
+
+# Paranoid Packages
+ifneq ($(TARGET_USES_AOSP_CAMERA),true)
+PRODUCT_PACKAGES += ParanoidCamera
+endif
+
+PRODUCT_PACKAGES += \
     Longshot \
     ParanoidPapers \
     ParanoidQuickStep \
-    ThemePicker
 
 ifneq ($(filter RELEASE BETA,$(PA_BUILDTYPE)),)
     PRODUCT_PACKAGES += \
@@ -51,29 +77,30 @@ endif
 PRODUCT_PACKAGES += \
     AOSPAOverlays
 
-# Snapdragon apps
+# QTI VNDK Framework Detect
 PRODUCT_PACKAGES += \
-    SnapdragonGallery
-
-# Abstruct
-PRODUCT_PACKAGES += Abstruct
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
 
 # Retro Music Player
 PRODUCT_PACKAGES += RetroMusicPlayer
 
-# CAF packages
-# TCP Connection Management
-PRODUCT_PACKAGES += tcmiface
-PRODUCT_BOOT_JARS += tcmiface
+# Snapdragon Apps
+PRODUCT_PACKAGES += \
+    SnapdragonGallery
 
-# Bluetooth Audio (A2DP)
-PRODUCT_PACKAGES += libbthost_if
+# Tools - FS
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.exfat \
+    mkfs.ntfs \
+    mount.ntfs
 
-# MSIM manual provisioning
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
-
-# Extra tools in PA
+# Tools - Misc
 PRODUCT_PACKAGES += \
     7z \
     awk \
@@ -94,16 +121,7 @@ PRODUCT_PACKAGES += \
     wget \
     zip
 
-# Filesystems tools
-PRODUCT_PACKAGES += \
-    fsck.exfat \
-    fsck.ntfs \
-    mke2fs \
-    mkfs.exfat \
-    mkfs.ntfs \
-    mount.ntfs
-
-# Openssh
+# Tools - openssh
 PRODUCT_PACKAGES += \
     scp \
     sftp \
@@ -113,6 +131,6 @@ PRODUCT_PACKAGES += \
     ssh-keygen \
     start-ssh
 
-# rsync
+# Tools - rsync
 PRODUCT_PACKAGES += \
     rsync
