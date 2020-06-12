562108a09e3aaabf28a98897f45a518fb0558083
PixelBoot <bootingpixels@gmail.com>
2020-06-13 00:34:52 +0200

    pa: add angler
    Signed-off-by: PixelBoot <bootingpixels@gmail.com>
    Change-Id: Ic3af4288522dd4b7f1e671681e1a2701007a01b5

---
+++ products/angler/pa_angler.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2016-2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (pa_angler,$(TARGET_PRODUCT))
+
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Get the defaults going.
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Inherit from the PA configuration.
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+# Override AOSP defaults that we want changed.
+PRODUCT_NAME := pa_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.014 4503998 release-keys"
+
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+
+endif
