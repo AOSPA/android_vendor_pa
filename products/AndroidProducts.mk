2fcf6e7fa9772ad06f335eda03204132b92cada9
PixelBoot <bootingpixels@gmail.com>
2020-06-13 00:34:52 +0200

    pa: add angler
    Signed-off-by: PixelBoot <bootingpixels@gmail.com>
    Change-Id: Ic3af4288522dd4b7f1e671681e1a2701007a01b5

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,6 +16,7 @@
 # Product Makefiles
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/A6020/pa_A6020.mk \
+    $(LOCAL_DIR)/angler/pa_angler.mk \
     $(LOCAL_DIR)/beryllium/pa_beryllium.mk \
     $(LOCAL_DIR)/cepheus/pa_cepheus.mk \
     $(LOCAL_DIR)/chiron/pa_chiron.mk \
@@ -44,6 +45,7 @@ PRODUCT_MAKEFILES += \
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     pa_A6020-userdebug \
+    pa_angler-userdebug \
     pa_beryllium-userdebug \
     pa_cepheus-userdebug \
     pa_chiron-userdebug \
