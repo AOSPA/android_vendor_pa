13ef7bbdcb3565a2e499bdd431447e39f5eba27a
Joshua Primero <jprimero155@gmail.com>
2020-06-17 21:47:22 +0000

    pa: Add ChromeModernPublic for ARM
    * Rename it To ChromeModernPublic because thats what we use
    * Updated both version to v83.0.4103.101
    * From: https://github.com/bromite/chromium/releases/tag/83.0.4103.101
    
    Change-Id: I83f37316c226e2c1a89dda863e58e0abec8259f7
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -1,14 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := ChromePublic
-LOCAL_SRC_FILES     := app/ChromePublic.apk
+LOCAL_MODULE        := ChromeModernPublic
+ifeq ($(TARGET_ARCH), arm64)
+LOCAL_SRC_FILES     := app/ChromeModernPublic_arm64.apk
+else
+LOCAL_SRC_FILES     := app/ChromeModernPublic_arm.apk
+endif
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := APPS
 LOCAL_CERTIFICATE   := PRESIGNED
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_OVERRIDES_PACKAGES := Browser Browser2
+ifeq ($(TARGET_ARCH), arm64)
+LOCAL_MULTILIB := 64
+else
 LOCAL_MULTILIB := 32
+endif
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
