13045d764c963f200a8ddec115954877e10ae144
adiatul16 <adiatul16@gmail.com>
2020-06-19 15:19:00 +0000

    vendor: added wayne to official
    Change-Id: I4449c55a2da8281bc095be9be35c47e731b22e0b

---
+++ products/wayne/pa_wayne.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2018 The Xiaomi-SDM660 Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+#TWRP
+BUILD_TWRP := false
+
+#AOSP SU
+WITH_SU := false
+
+#Build type
+CUSTOM_BUILD_TYPE=UNOFFICIAL
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/wayne/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+# Define first api level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+#GAPPS
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Build Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
+
+# Device identifier
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := pa_wayne
+PRODUCT_DEVICE := wayne
+PRODUCT_MODEL := MI 6X
