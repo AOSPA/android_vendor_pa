90bbe6bff7b807b2333a76b2c145cd2de6cd3aef
Alexander Koskovich <zvnexus@outlook.com>
2020-06-28 22:24:53 -0400

    pa: soong: Add TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS.
    Change-Id: Ibb4202eae237cc9f167707c45dd86a313d95d7be

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -12,6 +12,7 @@ $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PRO
 $(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
 $(call add_json_bool, Target_uses_color_metadata, $(filter true,$(TARGET_USES_COLOR_METADATA)))
+$(call add_json_bool, Target_uses_prebuilt_dynamic_partitions,   $(filter true,$(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
