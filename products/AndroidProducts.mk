4bb9beaee0e0a8a630e0b771ebf042967ae05920
adiatul16 <adiatul16@gmail.com>
2020-07-08 15:25:11 +0000

    pa: products: Add Xiaomi MI 6X
    Change-Id: I3dc3690321fa8f5f5d583281c81faf03f9e809c8

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -46,6 +46,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/rosy/pa_rosy.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
     $(LOCAL_DIR)/sakura/pa_sakura.mk \
+    $(LOCAL_DIR)/wayne/pa_wayne.mk \
     $(LOCAL_DIR)/whyred/pa_whyred.mk \
     $(LOCAL_DIR)/X00TD/pa_X00TD.mk \
     $(LOCAL_DIR)/X01BD/pa_X01BD.mk
@@ -82,6 +83,7 @@ COMMON_LUNCH_CHOICES += \
     pa_raphael-userdebug \
     pa_sagit-userdebug \
     pa_sakura-userdebug \
+    pa_wayne-userdebug \
     pa_whyred-userdebug \
     pa_X00TD-userdebug \
     pa_X01BD-userdebug
