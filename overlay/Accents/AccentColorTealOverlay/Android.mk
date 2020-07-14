cd776a456db42e888c021d14877eb49a43422b4c
Alexander Koskovich <zvnexus@outlook.com>
2020-07-14 17:33:58 -0400

    pa: overlay: Clean up RRO makefiles and manifests.
     * A lot of these are implied or not needed for RRO.
    
     * Please keep formatting the same across these, the
       NavigationBarModeGesturalOverlay* stuff from ArrowOS
       used different comments for their copyright header.
    
     * Removed some packages/configs for GApps related overlays,
       please set them in GApps from now on since we ship with
       them. There is likely more leftover GMS stuff but I wasn't
       going to inspect each one.
    
    Change-Id: Ie45fcc2cb58de495b95e16e8b63d48dcdcc53c5b

--- overlay/Accents/AccentColorTealOverlay/Android.mk
+++ overlay/Accents/AccentColorTealOverlay/Android.mk
@@ -16,11 +16,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := AccentColorTealOverlay
-LOCAL_MODULE_OWNER := aospa
-LOCAL_MODULE_TAGS := optional
-LOCAL_PRODUCT_MODULE := true
-LOCAL_CERTIFICATE := platform
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_RRO_THEME := AccentColorTeal
 LOCAL_SDK_VERSION := current
 include $(BUILD_RRO_PACKAGE)
