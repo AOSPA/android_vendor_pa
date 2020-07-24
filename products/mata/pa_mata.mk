cf71fc482a9421df62e4df6a1a2fa9c7421eea05
Alexander Koskovich <zvnexus@outlook.com>
2020-07-24 05:07:17 -0400

    pa: products: mata: Cleanup product definition.
    Change-Id: Idf63c4818e9ebd6536361adf853e2431dd64615e

--- products/mata/pa_mata.mk
+++ products/mata/pa_mata.mk
@@ -15,32 +15,17 @@
 # Ensure Essential PH-1 is the target product.
 ifeq (pa_mata,$(TARGET_PRODUCT))
 
-# Inherit some common aosp stuff.
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Vendor blobs
-$(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
-
-# Device
+# Inherit from our custom device configuration.
 $(call inherit-product, device/essential/mata/device.mk)
 
-# Inherit from the PA configuration.
+# Inherit from our AOSPA configuration.
 $(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
-# Recovery FStab
-TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
-
-# PA declarations.
 TARGET_BOOT_ANIMATION_RES := 1440
-
-# Exclude QCOM SEPolicy
-TARGET_EXCLUDE_QCOM_SEPOLICY := true
 
 # Device identifiers.
 PRODUCT_DEVICE := mata
@@ -48,15 +33,5 @@ PRODUCT_NAME := pa_mata
 PRODUCT_BRAND := Essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
-PRODUCT_RELEASE_NAME := mata
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=mata \
-        PRIVATE_BUILD_DESC="mata-userdebug 10 QQ1A.200105.088 590 test-keys"
-
-PRODUCT_ACTIONABLE_COMPATIBLE_PROPERTY_DISABLE := true
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-
-PRODUCT_CHARACTERISTICS := nosdcard
 
 endif
