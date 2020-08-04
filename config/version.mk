38d0552ef5fc75584b501a2906563667a3714747
Vishalcj17 <vishalcj@aospa.co>
2020-08-04 18:20:18 +0530

    pa: Quartz 4 is out!
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ib9a1d9c39426c99082bf1c0cbf15a906283ddaaf

--- config/version.mk
+++ config/version.mk
@@ -34,7 +34,7 @@ ifndef PA_VERSION_CODE
   # The version code is the upgradable portion during the cycle of
   # every major Android release. Each version code upgrade indicates
   # our own major release during each lifecycle.
-  PA_VERSION_CODE := 4
+  PA_VERSION_CODE := 5
 endif
 
 # Determines the variant of the build.
