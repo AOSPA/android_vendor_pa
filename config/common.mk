073dd6fada113825f0fd18e852debabe9250e8d8
Mesquita <mickaelmendes50@gmail.com>
2020-09-09 19:53:01 -0300

    pa: config: Update fingerprint to RP1A.200720.009.
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: I1988347d19a4dbb699c94048b2fa505c40b36a46

--- config/common.mk
+++ config/common.mk
@@ -122,7 +122,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Properties
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-BUILD_FINGERPRINT ?= google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT ?= google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \
