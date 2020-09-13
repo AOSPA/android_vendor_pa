98b624c5f7d5a04176c6664b169fde127d8427e5
Alexander Koskovich <zvnexus@outlook.com>
2020-09-13 11:19:39 -0700

    pa: config: Switch to the new GMS package for Android 11.
    Change-Id: I8aa968afa9d411f853e176a04978220b4429fe43

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,3 @@
-IS_PHONE := true
-
-# Inherit common Paranoid Android stuff
+# Inherit common Paranoid Android stuff.
 $(call inherit-product, vendor/pa/config/common.mk)
-
 $(call inherit-product, vendor/pa/config/telephony.mk)
