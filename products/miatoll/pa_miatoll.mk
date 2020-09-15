19eaa271d5333f69f13c2958532cb9563f0c51b2
Lokesh773 <lokeshkasamneni773@gmail.com>
2020-09-15 09:31:51 +0000

    products:Add miatoll
    Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>
    Change-Id: Ide514d774562f0d9a6cd9ea585e633d671aadca1

---
+++ products/miatoll/pa_miatoll.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+PRODUCT_NAME := pa_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6250
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
