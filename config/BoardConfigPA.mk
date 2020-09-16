de18193b58ab156c193974185fd607620a1518f1
Alexander Koskovich <zvnexus@outlook.com>
2020-09-16 09:44:58 -0700

    config: Move SEPolicy back to PA vendor.
    Change-Id: I8ba8538574795eaa39afe486f596dfb9db26c7b6

--- config/BoardConfigPA.mk
+++ config/BoardConfigPA.mk
@@ -6,5 +6,9 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 include vendor/pa/config/BoardConfigQcom.mk
 endif
 
+# SEPolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/pa/sepolicy/private
+
 # Soong
 include vendor/pa/config/BoardConfigSoong.mk
