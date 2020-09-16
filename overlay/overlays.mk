f4a7ed8b3298776e7fdd910a2f5ee5683ee05396
Andrey Sprynga <imolo4nik@gmail.com>
2020-09-17 06:58:26 +0700

    pa: add russian translate for SettingsIntelligence
    Change-Id: I77e1a18670846e90792a60bcd37d909fec01233f

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -28,6 +28,7 @@ PRODUCT_PACKAGES += \
     AOSPABluetoothOverlay \
     AOSPAFrameworksOverlay \
     AOSPAOverlayStub \
+    AOSPASettingsIntelligenceOverlay \
     AOSPASettingsOverlay \
     AOSPASystemUIOverlay \
     AOSPATelephonyOverlay \
