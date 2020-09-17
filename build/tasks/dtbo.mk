9495666e0f4c45610caf1c3b036783ea72189131
Alexander Koskovich <zvnexus@outlook.com>
2020-09-17 16:57:02 -0700

    pa: Switch to CAF's kernel build system.
    Change-Id: I6aad967b28ed187efaa3f206b3fbc0df6d39d218

--- build/tasks/dtbo.mk
+++ build/tasks/dtbo.mk
@@ -1,30 +1,38 @@
-ifneq ($(TARGET_NO_KERNEL),true)
+#----------------------------------------------------------------------
+# Generate device tree overlay image (dtbo.img)
+#----------------------------------------------------------------------
+ifneq ($(strip $(TARGET_NO_KERNEL)),true)
 ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DTBO)),true)
-
-ifneq ($(BOARD_CUSTOM_DTBOIMG_MK),)
-include $(BOARD_CUSTOM_DTBOIMG_MK)
-else
 
 MKDTIMG := $(HOST_OUT_EXECUTABLES)/mkdtimg$(HOST_EXECUTABLE_SUFFIX)
 
+INSTALLED_DTBOIMAGE_TARGET := $(PRODUCT_OUT)/prebuilt_dtbo.img
+
 # Most specific paths must come first in possible_dtbo_dirs
-possible_dtbo_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts $(KERNEL_OUT)/arch/arm/boot/dts
+possible_dtbo_dirs = $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/dts $(KERNEL_OUT)/arch/arm/boot/dts
+$(shell mkdir -p $(possible_dtbo_dirs))
+dtbo_dir = $(firstword $(wildcard $(possible_dtbo_dirs)))
+dtbo_objs = $(shell find $(dtbo_dir) -name \*.dtbo)
 
 define build-dtboimage-target
-    $(call pretty,"Target dtbo image: $(BOARD_PREBUILT_DTBOIMAGE)")
-    $(hide) for dir in $(possible_dtbo_dirs); do \
-                if [ -d "$$dir" ]; then \
-                    dtbo_dir="$$dir"; \
-                    break; \
-                fi; \
-            done; \
-            $(MKDTIMG) create $@ --page_size=$(BOARD_KERNEL_PAGESIZE) $$(find "$$dtbo_dir" -name '*.dtbo')
+    $(call pretty,"Target dtbo image: $(INSTALLED_DTBOIMAGE_TARGET)")
+    $(hide) $(MKDTIMG) create $@ --page_size=$(BOARD_KERNEL_PAGESIZE) $(dtbo_objs)
     $(hide) chmod a+r $@
+    $(hide) $(AVBTOOL) add_hash_footer \
+	    --image $@ \
+	    --partition_size $(BOARD_DTBOIMG_PARTITION_SIZE) \
+	    --partition_name dtbo $(INTERNAL_AVB_SIGNING_ARGS)
 endef
 
-$(BOARD_PREBUILT_DTBOIMAGE): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET)
+ifeq ($(BOARD_AVB_ENABLE),true)
+$(INSTALLED_DTBOIMAGE_TARGET): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET) $(AVBTOOL)
 	$(build-dtboimage-target)
+endif
 
-endif # BOARD_CUSTOM_DTBOIMG_MK
-endif # BOARD_KERNEL_SEPARATED_DTBO
-endif # TARGET_NO_KERNEL
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTBOIMAGE_TARGET)
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_DTBOIMAGE_TARGET)
+endif
+endif
+
+.PHONY: dtboimage
+dtboimage: $(INSTALLED_DTBOIMAGE_TARGET)
