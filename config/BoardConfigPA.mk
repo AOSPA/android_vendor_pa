2cedf43c77aafa990d6e4e193a2546f7fc237d76
Alexander Koskovich <zvnexus@outlook.com>
2020-09-17 16:57:02 -0700

    pa: Switch to CAF's kernel build system.
    Change-Id: I6aad967b28ed187efaa3f206b3fbc0df6d39d218

--- config/BoardConfigPA.mk
+++ config/BoardConfigPA.mk
@@ -1,6 +1,3 @@
-# Kernel
-include vendor/pa/config/BoardConfigKernel.mk
-
 # QCOM flags
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 include vendor/pa/config/BoardConfigQcom.mk
@@ -9,6 +6,3 @@ endif
 # SEPolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     vendor/pa/sepolicy/private
-
-# Soong
-include vendor/pa/config/BoardConfigSoong.mk
