05279f76f42c2d7fe105266235a808bd83b77604
Anay Wadhera <awadhera@berkeley.edu>
2020-09-17 23:18:06 -0700

    mata: update to ruby
    Change-Id: Ie0bacee5efc2016470a939e132a5a79560622e9e

--- products/mata/pa_mata.mk
+++ products/mata/pa_mata.mk
@@ -17,24 +17,13 @@ ifeq (pa_mata,$(TARGET_PRODUCT))
 
 # Inherit some common aosp stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Vendor blobs
-$(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Device
-$(call inherit-product, device/essential/mata/device.mk)
+$(call inherit-product, device/essential/mata/mata.mk)
 
 # Inherit from the PA configuration.
 $(call inherit-product, vendor/pa/config/common_full_phone.mk)
-
-# Recovery FStab
-TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
 
 # PA declarations.
 TARGET_BOOT_ANIMATION_RES := 1440
@@ -53,10 +42,5 @@ PRODUCT_RELEASE_NAME := mata
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=mata \
         PRIVATE_BUILD_DESC="mata-userdebug 10 QQ1A.200105.088 590 test-keys"
-
-PRODUCT_ACTIONABLE_COMPATIBLE_PROPERTY_DISABLE := true
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-
-PRODUCT_CHARACTERISTICS := nosdcard
 
 endif
