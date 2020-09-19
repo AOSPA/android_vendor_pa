1cae4319569bae5bfa4870ee3ca480ccd6f1ab7c
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-19 22:14:26 +0530

    pa: envsetup: Stop executing vendor HAL script.
     * CAF used this to generate blueprints for their QTI
       vendor HALs under vendor/qcom/opensource/interfaces.
    
     * In Android 11 they switched to pre-generated blueprints,
       so there's no reason to run this at build time, also
       it slows down build.
    
     * Also remove mka, after removing this it no longer
       serves a purpose as the "m" function exists.
    
    Change-Id: I9644a08d120fbfedf0cb119e21e12800fa2509ff

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -7,7 +7,6 @@ Additional Paranoid Android functions:
 - aospremote:      Add git remote for matching AOSP repository.
 - cafremote:       Add git remote for matching CodeAurora repository.
 - githubremote:    Add git remote for AOSPA Github.
-- mka:             Builds using SCHED_BATCH on all processors.
 - repolastsync:    Prints date and time of last repo sync.
 EOF
 }
@@ -17,7 +16,7 @@ function brunch()
     breakfast $*
 
     if [ $? -eq 0 ]; then
-        mka bacon
+        m bacon
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -172,37 +171,6 @@ function githubremote()
 
     git remote add aospa https://github.com/AOSPA/$PROJECT
     echo "Remote 'aospa' created"
-}
-
-# Make using all available CPUs
-function mka() {
-    if [ -f $ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh ]; then
-        vendor_hal_script=$ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh
-        source $vendor_hal_script --check
-        regen_needed=$?
-    else
-        vendor_hal_script=$ANDROID_BUILD_TOP/device/qcom/common/vendor_hal_makefile_generator.sh
-        regen_needed=1
-    fi
-
-    if [ $regen_needed -eq 1 ]; then
-        _wrap_build $(get_make_command hidl-gen) hidl-gen ALLOW_MISSING_DEPENDENCIES=true
-        RET=$?
-        if [ $RET -ne 0 ]; then
-            echo -n "${color_failed}#### hidl-gen compilation failed, check above errors"
-            echo " ####${color_reset}"
-            return $RET
-        fi
-        source $vendor_hal_script
-        RET=$?
-        if [ $RET -ne 0 ]; then
-            echo -n "${color_failed}#### HAL file .bp generation failed dure to incpomaptible HAL files , please check above error log"
-            echo " ####${color_reset}"
-            return $RET
-        fi
-    fi
-
-    m -j "$@"
 }
 
 function repolastsync() {
