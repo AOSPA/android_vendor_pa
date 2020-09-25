b138cd456d0fd0adab9ea6a0ca79d41c89f78def
Jake Weinstein <jake@aospa.co>
2020-09-25 23:15:57 +0200

    pa: Add Nubia Mini 5G
    Change-Id: Iea62c9cf8b703d7e7d8439c9174b573824b6bc5b

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -26,6 +26,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/jasmine_sprout/pa_jasmine_sprout.mk \
     $(LOCAL_DIR)/laurel_sprout/pa_laurel_sprout.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
+    $(LOCAL_DIR)/mini5g/pa_mini5g.mk \
     $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
     $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
     $(LOCAL_DIR)/oneplus7/pa_oneplus7.mk \
@@ -56,6 +57,7 @@ COMMON_LUNCH_CHOICES += \
     pa_jasmine_sprout-userdebug \
     pa_laurel_sprout-userdebug \
     pa_mata-userdebug \
+    pa_mini5g-userdebug \
     pa_oneplus6-userdebug \
     pa_oneplus7-userdebug \
     pa_oneplus7pro-userdebug \
