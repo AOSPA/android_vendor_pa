78a23106b6f94ad452862bbb20b2dc76b17b5fbe
Mesquita <mesquita@aospa.co>
2020-10-10 12:23:32 -0300

    pa: products: Add Poco F2 Pro (lmi)
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: I86cc3895a9236f8a54122c748d7fd77ebe03083d

---
+++ products/lmi/pa_lmi.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_lmi,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/lmi/device.mk)
+
+# Inherit common PA configuration
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+# Override build properties
+PRODUCT_NAME := pa_lmi
+PRODUCT_DEVICE := lmi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := F2 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+endif
