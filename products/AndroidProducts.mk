3794f1146dc223e008f3962690f329992d2e04cd
Mesquita <mickaelmendes50@gmail.com>
2020-10-10 12:23:32 -0300

    pa: products: Add Poco F2 Pro (lmi)
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: I86cc3895a9236f8a54122c748d7fd77ebe03083d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -33,6 +33,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/land/pa_land.mk \
     $(LOCAL_DIR)/laurel_sprout/pa_laurel_sprout.mk \
     $(LOCAL_DIR)/lavender/pa_lavender.mk \
+    $(LOCAL_DIR)/lmi/pa_lmi.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
     $(LOCAL_DIR)/mido/pa_mido.mk \
     $(LOCAL_DIR)/onc/pa_onc.mk \
@@ -74,6 +75,7 @@ COMMON_LUNCH_CHOICES += \
     pa_land-userdebug \
     pa_laurel_sprout-userdebug \
     pa_lavender-userdebug \
+    pa_lmi-userdebug \
     pa_mata-userdebug \
     pa_mido-userdebug \
     pa_onc-userdebug \
