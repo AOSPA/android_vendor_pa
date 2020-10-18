80a51a6cae31e63f6d778b547d1fbf0fd425eebb
Jake Weinstein <jake@aospa.co>
2020-10-18 20:52:59 +0200

    ROG Phone 3 (obiwan)
    Change-Id: Ieb8eb237b0232a08b8fbe502ae564e9056b1b505

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -29,6 +29,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/laurel_sprout/pa_laurel_sprout.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
     $(LOCAL_DIR)/mido/pa_mido.mk \
+    $(LOCAL_DIR)/obiwan/pa_obiwan.mk \
     $(LOCAL_DIR)/onc/pa_onc.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
@@ -64,6 +65,7 @@ COMMON_LUNCH_CHOICES += \
     pa_laurel_sprout-userdebug \
     pa_mata-userdebug \
     pa_mido-userdebug \
+    pa_obiwan-userdebug \
     pa_onc-userdebug \
     pa_oneplus3-userdebug \
     pa_oneplus6-userdebug \
