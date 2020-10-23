cd283e769a1d9ddace2147db2459a18ddcaaae88
Alexander Koskovich <zvnexus@outlook.com>
2020-10-23 09:37:44 -0700

    pa: Allow fingerprint module ID to be overwritten.
    Change-Id: I6cfd6b99db25235f746e389cfe74d0d120a2949d

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -15,6 +15,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += aospaGlobalVars
 SOONG_CONFIG_aospaGlobalVars += \
+    board_fingerprint_vendor \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
@@ -25,6 +26,7 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
 
 # Soong value variables
+SOONG_CONFIG_aospaGlobalVars_board_fingerprint_vendor := $(BOARD_FINGERPRINT_VENDOR)
 SOONG_CONFIG_aospaGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_aospaGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_aospaGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
