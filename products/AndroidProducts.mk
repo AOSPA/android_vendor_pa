2d5c5e0942b2690496c1d15d1399fb5bf0f2dd68
Vishalcj17 <vishalcj@aospa.co>
2020-12-01 09:36:11 +0530

    products: remove deprecated devices. (2/2)
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I06a839850dadc891ccce30cde8e8678b35cbc43c

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -29,7 +29,6 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/laurel_sprout/pa_laurel_sprout.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
     $(LOCAL_DIR)/mido/pa_mido.mk \
-    $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
     $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
     $(LOCAL_DIR)/oneplus7/pa_oneplus7.mk \
@@ -44,8 +43,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
     $(LOCAL_DIR)/wayne/pa_wayne.mk \
     $(LOCAL_DIR)/whyred/pa_whyred.mk \
-    $(LOCAL_DIR)/X00TD/pa_X00TD.mk \
-    $(LOCAL_DIR)/X01BD/pa_X01BD.mk
+    $(LOCAL_DIR)/X00TD/pa_X00TD.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
@@ -63,7 +61,6 @@ COMMON_LUNCH_CHOICES += \
     pa_laurel_sprout-userdebug \
     pa_mata-userdebug \
     pa_mido-userdebug \
-    pa_oneplus3-userdebug \
     pa_oneplus6-userdebug \
     pa_oneplus7-userdebug \
     pa_oneplus7pro-userdebug \
@@ -77,5 +74,4 @@ COMMON_LUNCH_CHOICES += \
     pa_sagit-userdebug \
     pa_wayne-userdebug \
     pa_whyred-userdebug \
-    pa_X00TD-userdebug \
-    pa_X01BD-userdebug
+    pa_X00TD-userdebug
