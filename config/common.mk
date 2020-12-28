5558ad4ca00d43f32bc6f6a9cadba95d68cf99d0
Alexander Koskovich <zvnexus@outlook.com>
2020-12-28 02:28:39 -0700

    pa: config: Build with Google modules if they exist.
    Change-Id: I6c7190328fe33537dd02f9aa324135460c7b958f

--- config/common.mk
+++ config/common.mk
@@ -50,6 +50,7 @@ ifneq ($(TARGET_DISABLES_GMS), true)
 
 # Inherit GMS, Pixel Features, and Modules.
 $(call inherit-product, vendor/google/gms/config.mk)
+$(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_r.mk)
 
 # Don't preoptimize prebuilts when building GMS.
 DONT_DEXPREOPT_PREBUILTS := true
