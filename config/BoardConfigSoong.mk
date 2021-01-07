9af349356a7a2fe285e678dc01c60930f950319d
StyloGey <stylogey@gmail.com>
2021-01-07 01:54:14 +0100

    pa: introduce vendor.aospa.power for gestures
    Change-Id: Icd7222888835d939da8032ace0fbdbb9fb555329

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -22,7 +22,23 @@ SOONG_CONFIG_aospaGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_fod_lib
+    target_surfaceflinger_fod_lib \
+    target_gesture_node \
+    target_single_tap_node \
+    target_double_tap_node \
+    target_v_node \
+    target_inverse_v_node \
+    target_o_node \
+    target_m_node \
+    target_w_node \
+    target_s_node \
+    target_arrow_left_node \
+    target_arrow_right_node \
+    target_up_node \
+    target_right_node \
+    target_down_node \
+    target_left_node \
+    target_two_finger_swipe_node
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -35,6 +51,22 @@ SOONG_CONFIG_aospaGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_aospaGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_aospaGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_aospaGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_aospaGlobalVars_target_gesture_node = $(patsubst "%",%,$(TARGET_GESTURES_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_single_tap_node = $(patsubst "%",%,$(TARGET_SINGLE_TAP_TO_WAKE_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_double_tap_node = $(patsubst "%",%,$(TARGET_TAP_TO_WAKE_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_v_node = $(patsubst "%",%,$(TARGET_DRAW_V_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_inverse_v_node = $(patsubst "%",%,$(TARGET_DRAW_INVERSE_V_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_o_node = $(patsubst "%",%,$(TARGET_DRAW_O_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_m_node = $(patsubst "%",%,$(TARGET_DRAW_M_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_w_node = $(patsubst "%",%,$(TARGET_DRAW_W_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_s_node = $(patsubst "%",%,$(TARGET_DRAW_S_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_arrow_left_node = $(patsubst "%",%,$(TARGET_DRAW_ARROW_LEFT_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_arrow_right_node = $(patsubst "%",%,$(TARGET_DRAW_ARROW_RIGHT_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_up_node = $(patsubst "%",%,$(TARGET_ONE_FINGER_SWIPE_UP_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_right_node = $(patsubst "%",%,$(TARGET_ONE_FINGER_SWIPE_RIGHT_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_down_node = $(patsubst "%",%,$(TARGET_ONE_FINGER_SWIPE_DOWN_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_left_node = $(patsubst "%",%,$(TARGET_ONE_FINGER_SWIPE_LEFT_NODE))
+SOONG_CONFIG_aospaGlobalVars_target_two_finger_swipe_node = $(patsubst "%",%,$(TARGET_TWO_FINGER_SWIPE_NODE))
 
 # Qualcomm variables
 SOONG_CONFIG_NAMESPACES += aosp_vs_qva
