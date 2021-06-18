cef8a7aa586f43acc6d2ff2f0b418c0cdbb778bb
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-06-19 02:27:48 +0530

    DONOTMERGE pa: Add moar font overlays
    will add more soon
    
    Inter : v3.19
    Manrope : v4.504
    Urbanist : v1.251
    
    A variable font (VF) is a font file that is able to store a continuous range of design variants.
    The fonts are licensed under the Open Font License (OFL).
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: Id6088b641d227e8f4675ab9ec1350039594c879c

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -39,6 +39,9 @@ PRODUCT_PACKAGES += \
     AOSPATelephonyOverlay \
     AOSPAThemePickerOverlay \
     FontGoogleSansOverlay \
+    FontInterOverlay \
+    FontManropeOverlay \
+    FontUrbanistOverlay \
     ParanoidDozeOverlay \
     ParanoidHubOverlay \
     ParanoidLauncherOverlay
