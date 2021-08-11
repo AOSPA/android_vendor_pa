e89c842de791009c166bd35db35478a0eb25a939
Vishalcj17 <vishalcj@aospa.co>
2021-08-12 02:18:02 +0900

    aospa: Cleanup product makefiles.
    Change-Id: I4fbc99732581131adff720858ad668f0cdc6a562

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2020 Paranoid Android
+# Copyright (C) 2016-2021 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,67 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-# Product Makefiles
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/angler/pa_angler.mk \
-    $(LOCAL_DIR)/beryllium/pa_beryllium.mk \
-    $(LOCAL_DIR)/cepheus/pa_cepheus.mk \
-    $(LOCAL_DIR)/chiron/pa_chiron.mk \
-    $(LOCAL_DIR)/davinci/pa_davinci.mk \
-    $(LOCAL_DIR)/dipper/pa_dipper.mk \
-    $(LOCAL_DIR)/equuleus/pa_equuleus.mk \
-    $(LOCAL_DIR)/ginkgo/pa_ginkgo.mk \
-    $(LOCAL_DIR)/jasmine_sprout/pa_jasmine_sprout.mk \
-    $(LOCAL_DIR)/laurel_sprout/pa_laurel_sprout.mk \
-    $(LOCAL_DIR)/mata/pa_mata.mk \
-    $(LOCAL_DIR)/miatoll/pa_miatoll.mk \
-    $(LOCAL_DIR)/mini5g/pa_mini5g.mk \
-    $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
-    $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
-    $(LOCAL_DIR)/oneplus7/pa_oneplus7.mk \
-    $(LOCAL_DIR)/oneplus7pro/pa_oneplus7pro.mk \
-    $(LOCAL_DIR)/oneplus7t/pa_oneplus7t.mk \
-    $(LOCAL_DIR)/oneplus7tpro/pa_oneplus7tpro.mk \
-    $(LOCAL_DIR)/oneplus8/pa_oneplus8.mk \
-    $(LOCAL_DIR)/oneplus8pro/pa_oneplus8pro.mk \
-    $(LOCAL_DIR)/oneplus8t/pa_oneplus8t.mk \
-    $(LOCAL_DIR)/oneplus9/pa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/pa_oneplus9pro.mk \
-    $(LOCAL_DIR)/polaris/pa_polaris.mk \
-    $(LOCAL_DIR)/raphael/pa_raphael.mk \
-    $(LOCAL_DIR)/sagit/pa_sagit.mk \
-    $(LOCAL_DIR)/wayne/pa_wayne.mk \
-    $(LOCAL_DIR)/X00TD/pa_X00TD.mk
-
-# Lunch targets
-COMMON_LUNCH_CHOICES += \
-    pa_angler-userdebug \
-    pa_beryllium-userdebug \
-    pa_cepheus-userdebug \
-    pa_chiron-userdebug \
-    pa_davinci-userdebug \
-    pa_dipper-userdebug \
-    pa_equuleus-userdebug \
-    pa_ginkgo-userdebug \
-    pa_jasmine_sprout-userdebug \
-    pa_laurel_sprout-userdebug \
-    pa_mata-userdebug \
-    pa_miatoll-userdebug \
-    pa_mini5g-userdebug \
-    pa_oneplus6-userdebug \
-    pa_oneplus7-userdebug \
-    pa_oneplus7pro-userdebug \
-    pa_oneplus7t-userdebug \
-    pa_oneplus7tpro-userdebug \
-    pa_oneplus8-userdebug \
-    pa_oneplus8pro-userdebug \
-    pa_oneplus8t-userdebug \
-    pa_oneplus9-userdebug \
-    pa_oneplus9pro-userdebug \
-    pa_polaris-userdebug \
-    pa_raphael-userdebug \
-    pa_sagit-userdebug \
-    pa_wayne-userdebug \
-    pa_X00TD-userdebug
