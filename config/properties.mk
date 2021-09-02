b5ff41c4a00be0766535338da5937d6d952d771b
Mesquita <mesquita@aospa.co>
2021-09-02 20:19:46 -0300

    pa: config: Update fingerprint to Pixel 5. (RQ3A.210805.001)
    Change-Id: Iaf59da863da44f669cfdd7e67f9dad5b085bbb69
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- config/properties.mk
+++ config/properties.mk
@@ -37,7 +37,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Fingerprint
 ifeq ($(TARGET_BUILD_VARIANT),user)
-BUILD_FINGERPRINT ?= google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT ?= google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 endif
 
 # Gestures
